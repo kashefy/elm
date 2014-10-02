8140dcdac238e213b009cef52b743c73c05fcc5e
Youssef Kashef <youssef.kashef@gmail.com>
2014-10-02 23:08:04 +0200

    start testing

---
+++ cmake/BuildTests.cmake
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------------------
+#  CMake function for building tests
+# 
+#  Recursively scans ${CMAKE_SOURCE_DIR} for source files according to filter (e.g. *unittest*)
+#  And builds them as part of a single binary for push-button execution
+#  Args:
+#    test_project (string): test project name
+#    filter (string): source file filter to use with CMake's file (GLOB_RECURSE ...)
+# ----------------------------------------------------------------------------
+
+FUNCTION (build_tests test_project filter)
+    message (STATUS "------- Test Project: ${test_project} ------- ")
+    message (STATUS "Scanning for files: ${filter} in folder: ${CMAKE_SOURCE_DIR}")
+    file (GLOB_RECURSE SRCS ${CMAKE_SOURCE_DIR}/${filter})
+
+    if (NOT BUILD_python)
+        string(REGEX REPLACE "${CMAKE_SOURCE_DIR}/modules/python/[^;]+;?" "" SRCS "${SRCS}")
+    endif (NOT BUILD_python)
+     
+    list (LENGTH SRCS nbTestFiles )
+    if (nbTestFiles GREATER 0)
+        message (STATUS  "Identified ${nbTestFiles} test source files to build.")
+        add_executable (${test_project} ${SRCS})
+
+        if (BUILD_python)
+
+            add_definitions(-DPYTHON_USE_NUMPY=1)
+            add_dependencies( ${test_project} python )
+            include_directories( ${BIND_PYTHON_INCLUDE_DIRS} )
+            target_link_libraries(${test_project} ${${ROOT_PROJECT}_LIBS} ${GTEST_LIBRARIES} ${GTEST_MAIN_LIBRARIES} ${BIND_PYTHON_LIBS} python)
+
+        else (BUILD_python)
+
+            add_dependencies (${test_project} ${${ROOT_PROJECT}_MODULES})
+            target_link_libraries (${test_project} ${${ROOT_PROJECT}_LIBS} ${GTEST_LIBRARIES} ${GTEST_MAIN_LIBRARIES} ${${ROOT_PROJECT}_MODULES})
+
+        endif (BUILD_python)
+
+    else (nbTestFiles GREATER 0)
+        message( WARNING  "Unable to locate any test files. No tests to add to this project")
+    endif (nbTestFiles GREATER 0)
+    
+ENDFUNCTION( build_tests )
