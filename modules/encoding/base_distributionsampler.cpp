f97ef3058a355b8e7ede04b1ec3c9a7f549e6e32
Youssef Kashef <youssef.kashef@gmail.com>
2014-10-06 19:24:57 +0200

    decrease cmake min version

---
+++ modules/encoding/base_distributionsampler.cpp
@@ -0,0 +1,5 @@
+#include "base_distributionsampler.h"
+
+base_DistributionSampler::base_DistributionSampler()
+{
+}
---
+++ modules/encoding/base_distributionsampler.h
@@ -0,0 +1,10 @@
+#ifndef BASE_DISTRIBUTIONSAMPLER_H
+#define BASE_DISTRIBUTIONSAMPLER_H
+
+class base_DistributionSampler
+{
+public:
+    base_DistributionSampler();
+};
+
+#endif // BASE_DISTRIBUTIONSAMPLER_H
