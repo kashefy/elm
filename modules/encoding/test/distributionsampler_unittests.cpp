3a48b63e5d972879dd6ad47b1e5bf5d15bde09fd
Youssef Kashef <youssef.kashef@gmail.com>
2014-10-07 16:14:40 +0200

    test distribution sampler on gaussian pdf

--- modules/encoding/test/distributionsampler_unittests.cpp
+++ modules/encoding/test/distributionsampler_unittests.cpp
@@ -1,6 +1,7 @@
 #include "encoding/distributionsampler.h"
 
 #include "ts/ts.h"
+#include <opencv2/imgproc/imgproc.hpp>
 
 namespace {
 
@@ -26,6 +27,43 @@ TEST(DistrSampler1D, Uniform) {
     cv::Mat m, s;
     cv::meanStdDev(hist, m, s);
     EXPECT_NEAR(s.at<float>(0, 0), 0, 1e-5);
+}
+
+TEST(DistrSampler1D, Gaussian) {
+
+    const int SIZE = 10, N=2000;
+    const float MEAN = 5.f, STD_DEV = 2.f;
+
+    // generate gaussian pdf
+    cv::Mat data(1, N, CV_32FC1);
+    for(int i=0; i<N; i++) {
+
+        cv::randn(data, MEAN, STD_DEV);
+    }
+
+    float pdf_values[SIZE] = {};
+    for (int i=0; i<N; i++) {
+
+        float v = data.at<float>(i);
+        if ( v >= 0.f && v<10.0) {
+            ++pdf_values[static_cast<int>(v)];
+        }
+    }
+
+    DistributionSampler1D to; // test object
+    cv::Mat pdf = cv::Mat(1,10, CV_32FC1, pdf_values)/static_cast<float>(N)*SIZE;
+    to.pdf(pdf);
+
+    MatF hist = MatF::zeros(1, SIZE);
+
+    for(int i=0; i<N; i++) {
+
+        int sampled_index = to.Sample();
+        EXPECT_IN_CLOSED(sampled_index, 0, SIZE-1);
+        hist(sampled_index)++;
+    }
+
+    EXPECT_MAT_NEAR(hist/static_cast<float>(N)*SIZE, pdf, 0.2f);
 }
 
 } // namespace
