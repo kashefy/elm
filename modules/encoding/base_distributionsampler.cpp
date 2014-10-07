b4fcfbe5fb7c695292af3c5508ee21270c595571
Youssef Kashef <youssef.kashef@gmail.com>
2014-10-07 12:11:47 +0200

    normalize pdf for sampling

--- modules/encoding/base_distributionsampler.cpp
+++ modules/encoding/base_distributionsampler.cpp
@@ -1,12 +1,25 @@
 #include "base_distributionsampler.h"
 
+#include "core/mat_utils.h"
+
 using namespace cv;
 
-//cv::RNG base_DistributionSampler::rng_(0xFFFFFFFF);
+cv::RNG base_DistributionSampler::rng_(0xFFFFFFFF);
 
-void base_DistributionSampler::pdf(const MatF &pdf)
-{
+void base_DistributionSampler::pdf(const MatF &pdf) {
+
     pdf_ = pdf/sum(pdf)(0);
+    sem::CumSum(pdf_, pdf_);
+}
+
+float base_DistributionSampler::sample() const {
+
+    const float R = float(rng_);
+    int i = 0;
+    while(pdf_(i) < R ){
+        i++;
+    }
+    return pdf_(i);
 }
 
 base_DistributionSampler::base_DistributionSampler()
--- modules/encoding/base_distributionsampler.h
+++ modules/encoding/base_distributionsampler.h
@@ -8,12 +8,19 @@ class base_DistributionSampler
 public:
     virtual ~base_DistributionSampler() {}
 
+    /**
+     * @brief set probability density function.
+     * Prepare pdf for sampling by converting into a series of bin widths
+     * @param probability density function
+     */
     void pdf(const MatF &pdf);
+
+    float sample() const;
 
 protected:
     base_DistributionSampler();
 
-    //static cv::RNG rng_;
+    static cv::RNG rng_;
     MatF pdf_;
 
 };
