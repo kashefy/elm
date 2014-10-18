a0e3581593549f4192eabe497c7ba62e2a4bb020
Youssef Kashef <youssef.kashef@gmail.com>
2014-10-19 01:41:22 +0200

    wrap OpenCV function for computing gabor kernels

---
+++ modules/encoding/orientation.h
@@ -0,0 +1,25 @@
+#ifndef SEM_ENCODING_ORIENTATION_H_
+#define SEM_ENCODING_ORIENTATION_H_
+
+#include <core/typedefs.h>
+
+namespace sem {
+
+/**
+ * @brief Compute single gabor kernel
+ * Appropriate model for orientation selective V1 simple cells and edge detectors
+ *
+ * @param kernel radius, yields kernel of size (r*2+1, r*2+1)
+ * @param sigma Std dev of gaussian envelope
+ * @param theta orientation in radians
+ * @param lambd wavelength of sinusoid
+ * @param gamma spatial aspect ratio
+ * @param ps phase offset in radians
+ * @return kernel in 32-bit floats
+ */
+cv::Mat1f GaborKernel(int radius, float sigma, float theta, float lambd, float gamma, float ps);
+
+
+} // namespace sem
+
+#endif // SEM_ENCODING_ORIENTATION_H_
