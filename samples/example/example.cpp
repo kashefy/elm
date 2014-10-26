6272fe354c3875ec4460b82ffbd67fde26f564d3
Youssef Kashef <youssef.kashef@gmail.com>
2014-10-26 13:17:23 +0200

    start writing simulation for learning MNIST digits.

--- samples/example/example.cpp
+++ samples/example/example.cpp
@@ -5,8 +5,13 @@
 #include <opencv2/highgui.hpp>
 
 #include "core/core.h"
+#include "encoding/populationcode.h"
 #include "io/readmnist.h"
+#include "neuron/wtapoisson.h"
+#include "neuron/neuron.h"
+#include "neuron/zneuron.h"
 
+using namespace cv;
 using namespace std;
 namespace bfs=boost::filesystem;
 
@@ -16,10 +21,52 @@ int main() {
     ReadMNISTImages r;
     bfs::path p("C:\\Users\\woodstock\\dev\\data\\MNIST\\t10k-images.idx3-ubyte");
     r.ReadHeader(p.string().c_str());
+
+    MutexPopulationCode pop_code;
+
+    vector<std::shared_ptr<base_Learner> > layer_z;
+    WTAPoisson wta_z(1.f, 1000.f);
+
+    bool first_pass = true;
+
     while(!r.IS_EOF()) {
 
-        cv::imshow("i", r.Next());
-        cv::waitKey();
+        Mat img = r.Next();
+        cv::imshow("i", img);
+
+        pop_code.State(img);
+
+        Mat1f pc = pop_code.PopCode();
+
+        YNeuron neuron_y;
+        neuron_y.init(1.f, 1000.f);
+
+        Mat1f spikes_y(1, pc.total());
+
+        for(size_t i; i<pc.total(); i++) {
+
+            spikes_y(i) = neuron_y.State(pc(i));
+        }
+
+        if(first_pass) {
+
+            for(size_t i; i<spikes_y.total(); i++) {
+
+                shared_ptr<ZNeuron> p(new ZNeuron);
+                p->init(spikes_y.total(), 10);
+            }
+        }
+
+        Mat1f u(1, layer_z.size());
+        for(size_t i; i<layer_z.size(); i++) {
+
+            u(i) = layer_z[i]->Predict(spikes_y).at<float>(0);
+        }
+
+        wta_z.Compete(layer_z);
+
+        if(first_pass) { first_pass = false; }
+        waitKey();
     }
 
     return 0;
