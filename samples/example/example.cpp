638a78ecf0bbfce6cd852f927dc15f9de9df7271
Youssef Kashef <youssef.kashef@gmail.com>
2014-10-27 10:29:54 +0200

    fix for loop initializations

--- samples/example/example.cpp
+++ samples/example/example.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/highgui.hpp>
 
 #include "core/core.h"
+#include "core/mat_utils.h"
 #include "encoding/populationcode.h"
 #include "io/readmnist.h"
 #include "neuron/wtapoisson.h"
@@ -19,6 +20,7 @@ class Simulation
 {
 public:
     Simulation()
+        : nb_learners_(10)
     {
 
     }
@@ -35,13 +37,14 @@ public:
 
         bool first_pass = true;
 
+        int ri=0;
         while(!r.IS_EOF()) {
 
+            //cout<<"i"<<ri++<<endl;
             Mat img = r.Next();
-            cv::imshow("i", img);
+            //cv::imshow("i", img);
 
             pop_code.State(img);
-
             Mat1f pc = pop_code.PopCode();
 
             YNeuron neuron_y;
@@ -49,34 +52,40 @@ public:
 
             Mat1f spikes_y(1, pc.total());
 
-            for(size_t i; i<pc.total(); i++) {
+            for(size_t i=0; i<pc.total(); i++) {
 
                 spikes_y(i) = neuron_y.State(pc(i));
             }
 
             if(first_pass) {
 
-                for(size_t i; i<spikes_y.total(); i++) {
+                for(size_t i=0; i<nb_learners_; i++) {
 
                     shared_ptr<ZNeuron> p(new ZNeuron);
                     p->init(spikes_y.total(), 10);
+                    layer_z_.push_back(p);
                 }
             }
 
             Mat1f u(1, layer_z_.size());
-            for(size_t i; i<layer_z_.size(); i++) {
+            for(size_t i=0; i<layer_z_.size(); i++) {
 
                 u(i) = layer_z_[i]->Predict(spikes_y).at<float>(0);
             }
 
-            Mat1f spikes_z = wta_z.Compete(layer_z_);
-            for(size_t i; i<layer_z_.size(); i++) {
+            //cout<<"x6"<<layer_z_.size()<<endl;
+            Mat1i spikes_z = wta_z.Compete(layer_z_);
+            //cout<<spikes_z<<endl;
+            for(size_t i=0; i<layer_z_.size(); i++) {
 
-                layer_z_[i]->Learn(spikes_z.col(i) > 0);
+                //cout<<spikes_z.col(i)<<endl;
+                layer_z_[i]->Learn(spikes_z.col(i));
             }
 
+
+            //cout<<"x7"<<endl;
             if(first_pass) { first_pass = false; }
-            waitKey();
+            //waitKey();
         }
     }
 
@@ -90,12 +99,10 @@ public:
 
         WTAPoisson wta_z(1.f, 1000.f);
 
-        bool first_pass = true;
-
         while(!r.IS_EOF()) {
 
             Mat img = r.Next();
-            cv::imshow("i", img);
+            //cv::imshow("i", img);
 
             pop_code.State(img);
 
@@ -106,29 +113,37 @@ public:
 
             Mat1f spikes_y(1, pc.total());
 
-            for(size_t i; i<pc.total(); i++) {
+            for(size_t i=0; i<pc.total(); i++) {
 
                 spikes_y(i) = neuron_y.State(pc(i));
             }
 
-            if(first_pass) {
-
-                for(size_t i; i<spikes_y.total(); i++) {
-
-                    shared_ptr<ZNeuron> p(new ZNeuron);
-                    p->init(spikes_y.total(), 10);
-                }
-            }
-
             Mat1f u(1, layer_z_.size());
-            for(size_t i; i<layer_z_.size(); i++) {
+            for(size_t i=0; i<layer_z_.size(); i++) {
 
                 u(i) = layer_z_[i]->Predict(spikes_y).at<float>(0);
             }
 
-            Mat1f spikes_z = wta_z.Compete(layer_z_);
+            //Mat1f spikes_z = wta_z.Compete(layer_z_);
+            //waitKey();
+        }
+    }
 
-            if(first_pass) { first_pass = false; }
+    void Eval()
+    {
+        for(size_t i=0; i<layer_z_.size(); i++) {
+
+            Mat1f w = static_pointer_cast<ZNeuron>(layer_z_[i])->Weights();
+            Mat1f w_on(w.size());
+            Mat1f w_off(w.size());
+            for(size_t i=0, j=0; i<w.total(); i+=2, j++) {
+
+                w_on(j) = w(i);
+                w_off(j) = w(i+1);
+            }
+
+            imshow("on", sem::ConvertTo8U(w_on));
+            imshow("off", sem::ConvertTo8U(w_off));
             waitKey();
         }
     }
@@ -136,15 +151,27 @@ public:
 private:
     vector<std::shared_ptr<base_Learner> > layer_z_;
 
+    int nb_learners_;   ///< no. of learners (e.g. ZNeuron)
+
 };
 
 int main() {
 
-    std::cout<<sem::GetVersion();
+    cout<<sem::GetVersion()<<endl;
 
     Simulation s;
+
+    cout<<"Learn()"<<endl;
+
     s.Learn();
+
+    cout<<"Test()"<<endl;
+
     s.Test();
+
+    cout<<"Eval()"<<endl;
+
+    s.Eval();
 
     return 0;
 }
