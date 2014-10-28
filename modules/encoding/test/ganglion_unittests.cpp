c5e3e50f9a02c7905f7e7bd0f662eeafc273cc07
Youssef Kashef <youssef.kashef@gmail.com>
2014-10-28 18:54:24 +0200

    add tests around DoG kernel

--- modules/encoding/test/ganglion_unittests.cpp
+++ modules/encoding/test/ganglion_unittests.cpp
@@ -14,12 +14,14 @@ protected:
     {
         radius_ = 13;
         scale_ = 1.f;
-        to_.Init(radius_, scale_, true);
+        center_on_ = true;
+        to_.Init(radius_, scale_, center_on_);
     }
 
     DiffOfGaussians2dSq to_;    ///< test object
     int radius_;                ///< kernel radius
     float scale_;               ///< scale
+    bool center_on_;            ///< center on surround off
 };
 
 TEST_F(DiffOfGaussians2dSqTest, InvalidInit)
@@ -38,15 +40,35 @@ TEST_F(DiffOfGaussians2dSqTest, KernelProperties)
     EXPECT_MAT_TYPE(kernel, CV_32F);
 
     EXPECT_MAT_EQ(kernel, kernel.t());
+    Mat kernel_flipped;
+    flip(kernel, kernel_flipped, 0);
+    EXPECT_MAT_EQ(kernel, kernel_flipped);
+    flip(kernel, kernel_flipped, 1);
+    EXPECT_MAT_EQ(kernel, kernel_flipped);
+    flip(kernel.t(), kernel_flipped, 1);
+    EXPECT_MAT_EQ(kernel, kernel_flipped);
 }
 
 TEST_F(DiffOfGaussians2dSqTest, Kernel)
 {
     Mat kernel = to_.Kernel();
-    Mat mid_row = kernel.row(radius_);
-    Mat mid_col = kernel.row(radius_);
+    Mat1f mid_row = kernel.row(radius_);
+    Mat1f mid_col = kernel.col(radius_);
 
+    EXPECT_MAT_EQ(mid_col, mid_row.t());
 
+    double min_val, max_val;
+    int min_idx[2] = {-1, -1};
+    int max_idx[2] = {-1, -1};
+    minMaxIdx(mid_row, &min_val, &max_val, min_idx, max_idx);
+
+    EXPECT_NE(max_val, min_val);
+    EXPECT_GT(max_val, 0);
+    EXPECT_EQ(max_idx[1], kernel.cols/2);
+    EXPECT_LT(min_val, 0);
+    EXPECT_GE(mid_row(0), min_val);
+    EXPECT_GE(mid_row(mid_row.cols-1), min_val);
+    EXPECT_TRUE(max_idx[2]-abs(max_idx[2]-min_idx[2]) < kernel.cols/3);
 }
 
 TEST_F(DiffOfGaussians2dSqTest, OnOff)
