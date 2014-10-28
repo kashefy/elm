44dedf686cb723f04c636f256c0b7911f11acfe5
Youssef Kashef <youssef.kashef@gmail.com>
2014-10-28 20:55:03 +0200

    define base class for intensity contrast measure

---
+++ modules/encoding/intensitycontrast.cpp
@@ -0,0 +1,9 @@
+#include "encoding/intensitycontrast.h"
+
+base_IntensityContrast::~base_IntensityContrast()
+{
+}
+
+base_IntensityContrast::base_IntensityContrast()
+{
+}
---
+++ modules/encoding/intensitycontrast.h
@@ -0,0 +1,38 @@
+#ifndef SEM_ENCODING_INTENSITYCONTRAST_H_
+#define SEM_ENCODING_INTENSITYCONTRAST_H_
+
+#include <opencv2/core.hpp>
+
+/**
+ * @brief base class for measuring intensity contrast
+ */
+class base_IntensityContrast
+{
+public:
+    ~base_IntensityContrast();
+
+    /**
+     * @brief Initialize
+     * @param radius in pixels
+     * @param filter scale
+     */
+    virtual void Init(int radius, float scale) = 0;
+
+    /**
+     * @brief Compute intensity constrast from stimulus
+     * @param stimulus
+     */
+    virtual void Compute(cv::InputArray stimulus) = 0;
+
+    /**
+     * @brief Get Response
+     * @return response from most recent stimulus
+     */
+    virtual cv::Mat Response() = 0;
+
+protected:
+    base_IntensityContrast();
+
+};
+
+#endif // SEM_ENCODING_INTENSITYCONTRAST_H_
