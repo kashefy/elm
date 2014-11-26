55a7c4c2fdb3a675438cb38f302641d48c32142d
Youssef Kashef <youssef.kashef@gmail.com>
2014-11-26 15:49:30 +0100

    assertions for checking vector container is empty for convinience

---
+++ modules/ts/container.h
@@ -0,0 +1,25 @@
+/** Assertions around common container classes
+  */
+#ifndef SEM_TS_CONTAINER_H_
+#define SEM_TS_CONTAINER_H_
+
+#include "gtest/gtest.h"
+
+#include <vector>
+
+/**
+  Assert that container is empty
+  */
+template <class T>
+::testing::AssertionResult Empty(const std::vector< T > &container)
+{
+    if(container.empty()) { return ::testing::AssertionSuccess(); }
+    else {
+        return ::testing::AssertionFailure()
+                << "Container is not empty and contains"
+                << container.size() << " elements.";
+    }
+}
+#define EXPECT_EMPTY(x) EXPECT_TRUE( Empty(x) )
+
+#endif // SEM_TS_CONTAINER_H_
