b8bc3d0e2c5347fa576180e495ea04a4e23f57d7
Youssef Kashef <youssef.kashef@gmail.com>
2014-11-26 15:52:24 +0100

    add new header to general ts header

--- modules/ts/ts.h
+++ modules/ts/ts.h
@@ -1,3 +1,8 @@
+/** Include this header to include other headers with
+ * - custom assertions
+ * - test-related routines
+ * Don't forget to add new headers as you create them
+  */
 #ifndef SEM_TS_H_
 #define SEM_TS_H_
 
@@ -7,6 +12,7 @@
 
 #include "ts/interval.h"
 #include "ts/mat_assertions.h"
+#include "ts/container.h"
 
 const char* FullTestName(const ::testing::TestInfo *test_info);
 
