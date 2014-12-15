b2bd4af4da2ae49881fffafe1b0bd4dc003b7aee
Youssef Kashef <youssef.kashef@gmail.com>
2014-12-15 13:42:13 +0100

    iron out border values in NeighMeanVar() and add tests around it.

--- modules/core/mat_utils.cpp
+++ modules/core/mat_utils.cpp
@@ -64,26 +64,45 @@ Mat1i sem::Point2Mat(const Point2i &p)
     return m;
 }
 
-void sem::NeighMeanVar(const Mat1f &m, int radius, Mat1f &neigh_mean, Mat1f &neigh_var)
+void sem::NeighMeanVar(const Mat1f &m, int radius, Mat1f &neigh_mean, Mat1f &neigh_var,
+                       int border_type, const Scalar &value)
 {
-    neigh_mean = Mat1f(m.size());
-    neigh_var = Mat1f(m.size());
+    int diameter = radius*2+1;
 
-    for(int r=0; r<m.rows; r++) {
-
-        Mat1f sub_rows = m.rowRange(r < radius? 0 : r,
-                                    r+radius+1 > m.rows? m.rows : r);
-
-        for(int c=0; c<m.cols; c++) {
-
-            Mat1f sub_mat = sub_rows.rowRange(c < radius? 0 : c,
-                                               c+radius+1 > m.cols? m.cols : c);
-
-            Mat sub_mean, sub_stddev;
-            meanStdDev(sub_mat, sub_mean, sub_stddev);
-            neigh_mean(r, c) = static_cast<float>(sub_mean.at<double>(0));
-            neigh_var(r, c) = static_cast<float>(sub_stddev.at<double>(0)); // don't forget to square
-        }
+    // check radius
+    if(radius < 0) {
+        SEM_THROW_VALUE_ERROR("radius must be >= 0");
     }
-    multiply(neigh_var, neigh_var, neigh_var);
+    else if(radius == 0) {
+
+        // we can short-circuit the resuls for this case
+        neigh_mean = m.clone();
+        neigh_var = Mat1f::zeros(m.size());
+    }
+    else {
+        neigh_mean = Mat1f(m.size());
+        neigh_var = Mat1f(m.size());
+
+        // pad borders
+        Mat1f padded;
+        copyMakeBorder(m, padded,
+                       radius, radius, radius, radius,
+                       border_type, value);
+
+        for(int r=0; r<m.rows; r++) {
+
+            Mat1f sub_rows = padded.rowRange(r, r+diameter);
+
+            for(int c=0; c<m.cols; c++) {
+
+                Mat sub_mean, sub_stddev;
+                meanStdDev(sub_rows.colRange(c, c+diameter),
+                           sub_mean, sub_stddev);
+
+                neigh_mean(r, c) = static_cast<float>(sub_mean.at<double>(0));
+                neigh_var(r, c) = static_cast<float>(sub_stddev.at<double>(0)); // don't forget to square
+            }
+        }
+        multiply(neigh_var, neigh_var, neigh_var);
+    }
 }
--- modules/core/mat_utils.h
+++ modules/core/mat_utils.h
@@ -43,12 +43,19 @@ cv::Mat1i Point2Mat(const cv::Point2i& p);
 
 /**
   * @brief Calculate neighborhood variance
+  *
+  * Border elements are zero-padded
+  * \see http://docs.opencv.org/doc/tutorials/imgproc/imgtrans/copyMakeBorder/copyMakeBorder.html
+  *
   * @param[in] source matrix
   * @param[in] neighborhood radius
   * @param[out] matrix with neighborhood mean around each element
   * @param[out] matrix with neighborhood variance around each element
+  * @param[in] border type - \see OpenCV's copyMakeBorder() or borderInterpolate() for details.
+  * @param[in] value - Border value if border type==BORDER_CONSTANT
   */
-void NeighMeanVar(const cv::Mat1f& m, int radius, cv::Mat1f &neigh_mean, cv::Mat1f &neigh_var);
+void NeighMeanVar(const cv::Mat1f& m, int radius, cv::Mat1f &neigh_mean, cv::Mat1f &neigh_var,
+                  int border_type=cv::BORDER_REPLICATE, const cv::Scalar &value=cv::Scalar());
 
 /**
  * @brief Create Mat object (row vector) and fill with range
