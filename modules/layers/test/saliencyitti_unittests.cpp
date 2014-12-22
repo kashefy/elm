4d372d279003d0a012f9a488701a75abcaf160bb
Youssef Kashef <youssef.kashef@gmail.com>
2014-12-22 13:31:40 +0100

    remove display of images in unittest

--- modules/layers/test/saliencyitti_unittests.cpp
+++ modules/layers/test/saliencyitti_unittests.cpp
@@ -111,12 +111,12 @@ TEST_F(SaliencyIttiTest, Apply)
         sampled_saliency(loc_mat(1), loc_mat(0))++;
     }
 
-    imshow("sal", sem::ConvertTo8U(signal.MostRecent(NAME_SALIENCY_)));
-    imshow("sam", sem::ConvertTo8U(sampled_saliency));
-    waitKey();
+//    imshow("sal", sem::ConvertTo8U(signal.MostRecent(NAME_SALIENCY_)));
+//    imshow("sam", sem::ConvertTo8U(sampled_saliency));
+//    waitKey();
 }
 
-TEST_F(SaliencyIttiTest, DisplayApplyMNIST)
+TEST_F(SaliencyIttiTest, DISABLED_DisplayApplyMNIST)
 {
     ReadMNISTImages r;
     r.ReadHeader("/media/206CDC456CDC177E/Users/woodstock/dev/data/MNIST/t10k-images.idx3-ubyte");
