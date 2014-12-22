6f7fb2de24d61e3fb71181be4e3756527325e35d
Youssef Kashef <youssef.kashef@gmail.com>
2014-12-22 13:30:40 +0100

    mask low filter response regions from orientation conspicuity

--- modules/layers/saliencyitti.cpp
+++ modules/layers/saliencyitti.cpp
@@ -1,10 +1,13 @@
 #include "layers/saliencyitti.h"
 
 #include <iostream>
+
 #include <opencv2/imgproc.hpp>
 
-#include "core/signal.h"
+#include "core/defs.h"
 #include "core/mat_utils.h"
+#include "core/percentile.h"
+#include "core/signal.h"
 #include "encoding/orientation.h"
 
 using namespace std;
@@ -17,6 +20,12 @@ using namespace sem;
 const std::string SaliencyItti::KEY_INPUT_SCENE         = "scene";          ///< key to visual scene stimulus
 const std::string SaliencyItti::KEY_OUTPUT_SALIENCY     = "saliency";       ///< key to saliency measure
 const std::string SaliencyItti::KEY_OUTPUT_SALIENT_LOC  = "salient_loc";    ///< key to most recently attended location
+
+const float SaliencyItti::DEFAULT_SIGMA         = 3.f;
+const float SaliencyItti::DEFAULT_LAMBDA        = 10.f;
+const float SaliencyItti::DEFAULT_GAMMA         = 0.02f;
+const float SaliencyItti::DEFAULT_PS            = 0.f;    ///< [radians]
+const float SaliencyItti::DEFAULT_ORIENT_RESPONSE_PERCENTILE = 0.7f;
 
 SaliencyItti::~SaliencyItti()
 {
@@ -32,22 +41,22 @@ void SaliencyItti::Reset()
     saliency_ = Mat1f();
     stimulus_ = Mat1f();
 
-    const int RADIUS = 9;
-    const float SIGMA = 3;
-    const float _LAMBDA = 10;   //CV_PI;
-    const float GAMMA = 0.02;   //10;
-    const float PS = 0;         //CV_PI*0.5;
-
+    // orientation conspicuity
     theta_range_ = ARange_<float>(0.f, CV_PI, 90.*CV_PI/180.);
-
-    // Mat1f to VecF
-    const float* p = theta_range_.ptr<float>(0);
-    VecF theta(p, p+theta_range_.cols);
+    VecF theta = Mat_ToVec_<float>(theta_range_);
 
     gabors_.reset(new GaborFilterBank());
-    dynamic_cast<GaborFilterBank*>(gabors_.get())->Reset(RADIUS, SIGMA, theta, _LAMBDA, GAMMA, PS);
+    SEM_DYN_CAST(GaborFilterBank, gabors_)->Reset(DEFAULT_RADIUS,
+                                                  DEFAULT_SIGMA,
+                                                  theta,
+                                                  DEFAULT_LAMBDA,
+                                                  DEFAULT_GAMMA,
+                                                  DEFAULT_PS);
 
-    intensity_constrast_.Init(RADIUS, 1.f);
+    // intensity contrast
+    intensity_constrast_.Init(DEFAULT_RADIUS, 1.f);
+
+    percentile_orientation_response_ = DEFAULT_ORIENT_RESPONSE_PERCENTILE;
 }
 
 void SaliencyItti::Reconfigure(const LayerConfig &config)
@@ -68,6 +77,12 @@ void SaliencyItti::Apply()
     Mat orientation_spikes = pop_code_orient_.PopCode();
     Mat1i orientation_index(stimulus_.size());
 
+    Mat1f orientation_response_flat = Reshape(gabors_->Response()).reshape(1, 1);
+    float low_orientation_response_percentile = Percentile()
+            .CalcPercentile(orientation_response_flat,
+                            percentile_orientation_response_);
+    Mat1b mask_low_orientation_response(stimulus_.size());
+
     // assing orientation to each stimulus pixel
     size_t j_step = gabors_->size();
     double min_val;
@@ -76,6 +91,8 @@ void SaliencyItti::Apply()
 
         minMaxIdx(orientation_spikes.colRange(j, j+j_step), &min_val, 0, 0, max_idx);
         orientation_index(i) = max_idx[1];
+
+        mask_low_orientation_response(i) = orientation_response_flat(j+max_idx[1]) <= low_orientation_response_percentile;
     }
 
     // shift indices to be zero-centered.
@@ -86,14 +103,17 @@ void SaliencyItti::Apply()
     NeighMeanVar(orientation_index, 3, tmp, orientation_conspicuity);
     normalize(orientation_conspicuity, orientation_conspicuity, 0.f, 255.f, NORM_MINMAX, -1, noArray());
 
-    //TODO: clip away low response elements
-    //TODO: weigh by entropy to clip away uniform response regions
+    // apply feedback on orientation measurements
+    // clip away low response elements
+    orientation_conspicuity.setTo(0.f, mask_low_orientation_response);
+
+    //TODO: weigh by entropy to clip away uniform response regions, use as feedback signal
 
     intensity_constrast_.Compute(stimulus_);
     Mat intensity_constrast_norm = intensity_constrast_.Response();
     normalize(intensity_constrast_norm, intensity_constrast_norm, 0.f, 255.f, NORM_MINMAX, -1, noArray());
 
-    add(1.f*intensity_constrast_norm, 1.f*orientation_conspicuity, saliency_, noArray());
+    add(intensity_constrast_norm, orientation_conspicuity, saliency_);
 
     // generate 2d distribution from saliency map for later sampling of salient locations
     saliency_sampler_.pdf(saliency_);
