b07caac9e98d7df6110d9a3853856311c845edd4
Youssef Kashef <youssef.kashef@gmail.com>
2014-12-23 15:59:30 +0100

    turn InvalidParams test into value-parameterized test

--- modules/layers/test/layer_z_unittests.cpp
+++ modules/layers/test/layer_z_unittests.cpp
@@ -8,19 +8,30 @@ using namespace std;
 using namespace cv;
 using namespace sem;
 
+// name of keys in signal
+const string NAME_INPUT_SPIKES  = "in";          ///< no. of afferent spikes
+const string NAME_OUTPUT_SPIKES = "out";         ///< no. of output spikes/size of output layer
+const string NAME_OUTPUT_MEM_POT = "mem_pot";    ///< membrane potential
+
 /**
- * @brief class for testing layer Z initialization, the main, SEM learning algorithm
+ * @brief mixin for testing layer Z, the main, SEM learning algorithm
+ * Because we expect to test using fixutres as well as parametrized tests
+ * Since these two test types are derived differently mixins solves the problem of defining a common base
+ * The solution is adopted from this StackOverflow post:
+ * @see http://stackoverflow.com/questions/3152326/google-test-parameterized-tests-which-use-an-existing-test-fixture-class
+ *
+ * T specifies test type (fixture or parameterized)
  */
-class LayerZInitTest : public ::testing::Test
+template <class T> class LayerZTestBase : public T
 {
 protected:
     virtual void SetUp()
     {
         nb_afferents_ = 50;
-        PTree params;
-        params.put(LayerZ::PARAM_NB_AFFERENTS, nb_afferents_);
-        params.put(LayerZ::PARAM_NB_OUTPUT_NODES, 10);
-        config_.Params(params);
+        params_ = PTree();
+        params_.put(LayerZ::PARAM_NB_AFFERENTS, nb_afferents_);
+        params_.put(LayerZ::PARAM_NB_OUTPUT_NODES, 10);
+        config_.Params(params_);
 
         // IO
         config_.Input(LayerZ::KEY_INPUT_SPIKES, NAME_INPUT_SPIKES);
@@ -30,19 +41,21 @@ protected:
     }
 
     // members
-    LayerZ to_;             ///< test object
     LayerConfig config_;    ///< default layer configuration
+    PTree params_;          ///< default params
 
     int nb_afferents_;
-    // name of keys in signal
-    static const string NAME_INPUT_SPIKES;      ///< no. of afferent spikes
-    static const string NAME_OUTPUT_SPIKES;     ///< no. of output spikes/size of output layer
-    static const string NAME_OUTPUT_MEM_POT;    ///< membrane potential
 };
-// initialize static members
-const string LayerZInitTest::NAME_INPUT_SPIKES  = "in";
-const string LayerZInitTest::NAME_OUTPUT_SPIKES = "out";
-const string LayerZInitTest::NAME_OUTPUT_MEM_POT = "mem_pot";
+
+/**
+ * @brief class for testing layer z initialization routines
+ */
+class LayerZInitTest : public LayerZTestBase<testing::Test >
+{
+protected:
+    // members
+    LayerZ to_;  ///< test object
+};
 
 /**
  * @brief test validation of missing params
@@ -50,83 +63,10 @@ const string LayerZInitTest::NAME_OUTPUT_MEM_POT = "mem_pot";
 TEST_F(LayerZInitTest, MissingParams)
 {
     // remove key to output nodes
-    PTree params = config_.Params();
-    params.erase(LayerZ::PARAM_NB_OUTPUT_NODES);
-    config_.Params(params);
+    params_.erase(LayerZ::PARAM_NB_OUTPUT_NODES);
+    config_.Params(params_);
 
-    LayerZ to;
-    EXPECT_THROW(to.Reset(config_), std::exception);
-}
-
-/**
- * @brief test parameter validation
- */
-TEST_F(LayerZInitTest, InvalidParams)
-{
-    {
-        PTree params = config_.Params();
-        params.put(LayerZ::PARAM_DELTA_T, -0.1f);
-        config_.Params(params);
-
-        LayerZ to;
-        EXPECT_THROW(to.Reset(config_), ExceptionValueError);
-    }
-    {
-        PTree params = config_.Params();
-        params.put(LayerZ::PARAM_DELTA_T, 0.f);
-        config_.Params(params);
-
-        LayerZ to;
-        EXPECT_THROW(to.Reset(config_), ExceptionValueError);
-    }
-    {
-        PTree params = config_.Params();
-        params.put(LayerZ::PARAM_DELTA_T, -0.f);
-        config_.Params(params);
-
-        LayerZ to;
-        EXPECT_THROW(to.Reset(config_), ExceptionValueError);
-    }
-    {
-        PTree params = config_.Params();
-        params.put(LayerZ::PARAM_LEN_HISTORY, -3);
-        config_.Params(params);
-
-        LayerZ to;
-        EXPECT_THROW(to.Reset(config_), ExceptionValueError);
-    }
-    {
-        PTree params = config_.Params();
-        params.put(LayerZ::PARAM_LEN_HISTORY, 0);
-        config_.Params(params);
-
-        LayerZ to;
-        EXPECT_THROW(to.Reset(config_), ExceptionValueError);
-    }
-    {
-        PTree params = config_.Params();
-        params.put(LayerZ::PARAM_NB_AFFERENTS, 0);
-        config_.Params(params);
-
-        LayerZ to;
-        EXPECT_THROW(to.Reset(config_), ExceptionValueError);
-    }
-    {
-        PTree params = config_.Params();
-        params.put(LayerZ::PARAM_NB_AFFERENTS, -3);
-        config_.Params(params);
-
-        LayerZ to;
-        EXPECT_THROW(to.Reset(config_), ExceptionValueError);
-    }
-    {
-        PTree params = config_.Params();
-        params.put(LayerZ::PARAM_WTA_FREQ, -1);
-        config_.Params(params);
-
-        LayerZ to;
-        EXPECT_THROW(to.Reset(config_), ExceptionValueError);
-    }
+    EXPECT_THROW(LayerZ().Reset(config_), std::exception);
 }
 
 TEST_F(LayerZInitTest, IONames)
@@ -147,4 +87,36 @@ TEST_F(LayerZInitTest, IONames)
         LayerZ to;
         EXPECT_NO_THROW(to.IONames(cfg)) << "all required IO names present";
     }
+}
+
+typedef std::pair<std::string, float > TParamPairSF; /// convinience typdef to use as test params below
+
+/**
+ * @brief Parameterized test for testing layer z parameter value validation
+ * Using paramterized tests allows us to try different value more easily
+ */
+class LayerZParamsTest : public LayerZTestBase<testing::TestWithParam<TParamPairSF > >
+{
+protected:
+};
+
+// if you're using QTCreator, don't mind the ..._EvalGenerator_ warning at the bottom
+INSTANTIATE_TEST_CASE_P(TestWithParams,
+                        LayerZParamsTest,
+                        testing::Values(TParamPairSF(LayerZ::PARAM_DELTA_T, -1.f),
+                                        TParamPairSF(LayerZ::PARAM_DELTA_T, -0.f),
+                                        TParamPairSF(LayerZ::PARAM_DELTA_T, 0.f),
+                                        TParamPairSF(LayerZ::PARAM_LEN_HISTORY, -3),
+                                        TParamPairSF(LayerZ::PARAM_LEN_HISTORY, 0),
+                                        TParamPairSF(LayerZ::PARAM_NB_AFFERENTS, 0),
+                                        TParamPairSF(LayerZ::PARAM_NB_AFFERENTS, -3),
+                                        TParamPairSF(LayerZ::PARAM_WTA_FREQ, -0.001f),
+                                        TParamPairSF(LayerZ::PARAM_WTA_FREQ, -1.f)));
+
+TEST_P(LayerZParamsTest, InvalidParams)
+{
+    TParamPairSF tp = GetParam();
+    params_.put(tp.first, tp.second);
+    config_.Params(params_);
+    EXPECT_THROW(LayerZ().Reset(config_), ExceptionValueError);
 }
