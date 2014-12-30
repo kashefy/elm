bba96f80a21534389a7baceac58f2545e0cdc9cc
Youssef Kashef <youssef.kashef@gmail.com>
2014-12-30 21:11:37 +0100

    add credits

--- modules/layers/layerfactory.cpp
+++ modules/layers/layerfactory.cpp
@@ -19,7 +19,7 @@ typedef Registor_<base_Layer> LayerRegistor;
 typedef Registor_<base_Layer>::Registry LayerRegistry;
 
 /** Macros for adding individual instances to registry
- * credit: J.Turcot, T. Senechal, http://stackoverflow.com/questions/138600/initializing-a-static-stdmapint-int-in-c
+ *  credit: J. Turcot, T. Senechal, http://stackoverflow.com/questions/138600/initializing-a-static-stdmapint-int-in-c
  */
 #define ADD_TO_REGISTRY(Registor, NewInstance) (#NewInstance, &Registor::DerivedInstance<NewInstance>)
 #define ADD_TO_LAYER_REGISTRY(NewInstance) ADD_TO_REGISTRY(LayerRegistor, NewInstance)
