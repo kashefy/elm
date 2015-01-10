8508da646d14c9efef95baf6f906814be1dc50b5
Youssef Kashef <youssef.kashef@gmail.com>
2015-01-10 15:21:38 +0100

    add unit tests around cloud visitor

--- modules/core/test/featuredatavisitors_unittests.cpp
+++ modules/core/test/featuredatavisitors_unittests.cpp
@@ -10,7 +10,7 @@ using namespace sem;
 namespace {
 
 /**
- * @brief test class around FeatDataVisitorMatF
+ * @brief test class around FeatDataVisitorMat_f
  */
 class FeatDataVisitorMat_fTest : public ::testing::Test
 {
@@ -24,6 +24,9 @@ TEST_F(FeatDataVisitorMat_fTest, Empty)
     EXPECT_TRUE(to_(Mat_f(0, 0)).empty());
     EXPECT_TRUE(to_(Mat_f(1, 0)).empty());
     EXPECT_TRUE(to_(Mat_f(0, 1)).empty());
+
+    CloudXYZ::Ptr cld(new CloudXYZ);
+    EXPECT_TRUE(to_(cld).empty());
 }
 
 
@@ -33,9 +36,12 @@ TEST_F(FeatDataVisitorMat_fTest, EmptySize)
     EXPECT_MAT_DIMS_EQ(to_(Mat_f(0, 0)), Size2i(0, 0));
     EXPECT_MAT_DIMS_EQ(to_(Mat_f(1, 0)), Size2i(0, 1));
     EXPECT_MAT_DIMS_EQ(to_(Mat_f(0, 1)), Size2i(1, 0));
+
+    CloudXYZ::Ptr cld(new CloudXYZ);
+    EXPECT_MAT_DIMS_EQ(to_(cld), Size2i(0, 0));
 }
 
-TEST_F(FeatDataVisitorMat_fTest, MatF)
+TEST_F(FeatDataVisitorMat_fTest, FromMat_f)
 {
     int N=5;
 
@@ -54,7 +60,7 @@ TEST_F(FeatDataVisitorMat_fTest, MatF)
     }
 }
 
-TEST_F(FeatDataVisitorMat_fTest, Cloud)
+TEST_F(FeatDataVisitorMat_fTest, FromCloud)
 {
     Mat1f m(4, 3);
     randn(m, 0.f, 100.f);
@@ -86,79 +92,126 @@ TEST_F(FeatDataVisitorMat_fTest, Reset)
 }
 
 /**
- * @brief test class around FeatDataVisitorMatFTest
+ * @brief test class around FeatDataVisitorCloud
  */
 class FeatDataVisitorCloudTest : public ::testing::Test
 {
 protected:
-    FeatDataVisitorMat_f to_;    ///< test object
+    virtual void SetUp()
+    {
+        to_.Reset();
+    }
+
+    FeatDataVisitorCloud to_;    ///< test object
 };
 
 TEST_F(FeatDataVisitorCloudTest, Empty)
 {
-    EXPECT_TRUE(to_(Mat_f()).empty());
-    EXPECT_TRUE(to_(Mat_f(0, 0)).empty());
-    EXPECT_TRUE(to_(Mat_f(1, 0)).empty());
-    EXPECT_TRUE(to_(Mat_f(0, 1)).empty());
+    EXPECT_TRUE(to_(Mat_f())->empty());
+    EXPECT_TRUE(to_(Mat_f(0, 0))->empty());
+    EXPECT_TRUE(to_(Mat_f(1, 0))->empty());
+    EXPECT_TRUE(to_(Mat_f(0, 1))->empty());
+
+    CloudXYZ::Ptr cld(new CloudXYZ);
+    EXPECT_TRUE(to_(cld)->empty());
 }
 
 
 TEST_F(FeatDataVisitorCloudTest, EmptySize)
 {
-    EXPECT_MAT_DIMS_EQ(to_(Mat_f()), Size2i(0, 0));
-    EXPECT_MAT_DIMS_EQ(to_(Mat_f(0, 0)), Size2i(0, 0));
-    EXPECT_MAT_DIMS_EQ(to_(Mat_f(1, 0)), Size2i(0, 1));
-    EXPECT_MAT_DIMS_EQ(to_(Mat_f(0, 1)), Size2i(1, 0));
+    EXPECT_EQ(size_t(0), to_(Mat_f())->size());
+    EXPECT_EQ(size_t(0), to_(Mat_f(0, 0))->size());
+    EXPECT_EQ(size_t(0), to_(Mat_f(1, 0))->size());
+    EXPECT_EQ(size_t(0), to_(Mat_f(0, 1))->size());
+
+    CloudXYZ::Ptr cld(new CloudXYZ);
+    EXPECT_EQ(size_t(0), to_(cld)->size());
 }
 
-TEST_F(FeatDataVisitorCloudTest, MatF)
-{
-    int N=5;
-
-    while(N--) {
-
-        const int MAT_ROWS = abs(randu<int>()) % 10 + 1;
-        const int MAT_COLS = abs(randu<int>()) % 100 + 1;
-
-        Mat_f m(MAT_ROWS, MAT_COLS);
-        randn(m, 0.f, 100.f);
-
-        Mat_f m2 = to_(m);
-
-        EXPECT_MAT_EQ(m, m2) << "Matrices are not equal";
-        EXPECT_EQ(m.data, m2.data) << "Mats are not pointing to the same data in memory. Expecting shared copy.";
-    }
-}
-
-TEST_F(FeatDataVisitorCloudTest, Cloud)
+TEST_F(FeatDataVisitorCloudTest, FromMat_f)
 {
     Mat1f m(4, 3);
     randn(m, 0.f, 100.f);
-    CloudXYZ::Ptr cloud = Mat2PointCloud(m);
 
-    Mat_f m2 = to_(cloud);
+    CloudXYZ::Ptr cld = to_(m);
+    Mat1f m2 = PointCloud2Mat(cld);
     hconcat(m, Mat1f(m.rows, 1, 1), m);
 
     EXPECT_MAT_EQ(m, m2);
     EXPECT_NE(m.data, m2.data) << "Expecting deep copy. If intentionally optimized to be a shared copy, please update test.";
 }
 
-TEST_F(FeatDataVisitorCloudTest, Reset)
+TEST_F(FeatDataVisitorCloudTest, Cloud)
 {
-    EXPECT_NO_THROW(to_.Reset());
+    Mat1f m(4, 3);
+    randn(m, 0.f, 100.f);
+    CloudXYZ::Ptr cld = Mat2PointCloud(m);
 
+    CloudXYZ::Ptr cld2 = to_(cld);
+
+    EXPECT_EQ(cld->size(), cld2->size()) << "Size mismatch.";
+    EXPECT_EQ(cld->width, cld2->width) << "Width mismatch.";
+    EXPECT_EQ(cld->height, cld2->height) << "Height mismatch.";
+
+    for(CloudXYZ::iterator itr1=cld->begin(), itr2=cld2->begin();
+        itr1 != cld->end(); ++itr1, ++itr2) {
+
+        pcl::PointXYZ _p1 = *itr1;
+        pcl::PointXYZ _p2 = *itr2;
+        EXPECT_FLOAT_EQ(_p1.x, _p2.x) << "Unexpected value for x coordinate.";
+        EXPECT_FLOAT_EQ(_p1.y, _p2.y) << "Unexpected value for y coordinate.";
+        EXPECT_FLOAT_EQ(_p1.z, _p2.z) << "Unexpected value for z coordinate.";
+    }
+}
+
+TEST_F(FeatDataVisitorCloudTest, ResetValid)
+{
     Mat1f m(4, 3);
     randn(m, 0.f, 100.f);
 
-    Mat_f m2 = to_(m);
+    CloudXYZ::Ptr cld1 = to_(m);
     EXPECT_NO_THROW(to_.Reset());
 
-    CloudXYZ::Ptr cloud = Mat2PointCloud(m);
-    m2 = to_(cloud);
+    CloudXYZ::Ptr cld2 = Mat2PointCloud(m);
+    CloudXYZ::Ptr cld3 = to_(cld2);
     EXPECT_NO_THROW(to_.Reset());
 
-    m2 = to_(m);
+    cld1 = to_(m);
     EXPECT_NO_THROW(to_.Reset());
+}
+
+/**
+ * @brief Thest this visitor's caching
+ */
+TEST_F(FeatDataVisitorCloudTest, Reset)
+{
+    Mat1f m(4, 3, 1.f);
+    Mat1f m0 = m.clone();
+    hconcat(m0, Mat1f(m0.rows, 1, 1), m0);
+
+    CloudXYZ::Ptr cld = to_(m);
+    EXPECT_MAT_EQ(PointCloud2Mat(cld), m0);
+
+    cld = to_(m); // result of first call is cached.
+
+    const int N=2;
+    ASSERT_GT(N, 1) << "This test must iterate at least twice to see effect of Reset() on cached reference.";
+    for(int i=0; i<2; i++) {
+
+        cld = to_(m+1);
+
+        if(i == 0) {
+
+            EXPECT_MAT_EQ(PointCloud2Mat(cld), m0) << "Not suing cahced cloud.";
+        }
+        else {
+
+            Mat1f m2 = m0.clone();
+            m2.colRange(0, m2.cols-1).setTo(2.f);
+            EXPECT_MAT_EQ(PointCloud2Mat(cld), m2) << "Still using cached cloud";
+        }
+        to_.Reset();
+    }
 }
 
 } // annonymous namespace for visitors' test fixtures
