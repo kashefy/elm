4aa53ca8254bdc976b28006acefab3294cd07b3d
Youssef Kashef <youssef.kashef@gmail.com>
2015-01-12 17:54:57 +0100

    test VeVertices visitor with converting scalars

--- modules/core/test/visitors/visitorvecvertices_unittests.cpp
+++ modules/core/test/visitors/visitorvecvertices_unittests.cpp
@@ -27,8 +27,58 @@ protected:
     VisitorVecVertices to_;    ///< test object
 };
 
-TEST_F(VisitorVecVerticesTest, Empty)
+TEST_F(VisitorVecVerticesTest, EmptyVecVertices)
 {
+    EXPECT_TRUE(to_(VecVertices()).empty());
+}
+
+TEST_F(VisitorVecVerticesTest, EmptyVecVertices_Size)
+{
+    EXPECT_SIZE(0, to_(VecVertices()));
+}
+
+template<class T>
+class VisitorVecVerticesTypedTest : public VisitorVecVerticesTest
+{
+protected:
+    virtual void SetUp()
+    {
+        to_.Reset();
+    }
+
+    //typedef std::list<T> List;
+     static T shared_;
+     T value_;
+};
+
+/**
+ * @brief Value per type to use in fixtures below.
+ */
+template<class T>
+struct Twos_
+{
+    static const T val;
+};
+
+///< Register static variables to work with inside tests
+template<> const float Twos_<float>::val   = 2.f;
+template<> const int Twos_<int>::val     = 2;
+template<> const uchar Twos_<uchar>::val   = 2;
+template<> const Mat1f Twos_<Mat1f>::val   = Mat1f(1, 1, 2.f);
+
+typedef ::testing::Types<Mat1f, float, int, uchar> VisitorTypes;
+TYPED_TEST_CASE(VisitorVecVerticesTypedTest, VisitorTypes);
+
+TYPED_TEST(VisitorVecVerticesTypedTest, Twos)
+{
+    TypeParam in = Twos_<TypeParam >::val;
+
+    VecVertices vv = to_(in);
+
+    EXPECT_SIZE(1, vv);
+    EXPECT_SIZE(1, vv[0].vertices);
+
+    EXPECT_EQ(static_cast<uint32_t>(2), vv[0].vertices[0]);
 }
 
 } // annonymous namespace for VecVertices visitors' test fixtures
