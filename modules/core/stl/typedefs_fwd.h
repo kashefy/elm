3ece22a896dad4f118a27054b4bc8fa3f6b42615
Youssef Kashef <youssef.kashef@gmail.com>
2015-01-14 00:44:58 +0100

    remove unused commented code

--- modules/core/stl/typedefs_fwd.h
+++ modules/core/stl/typedefs_fwd.h
@@ -1,20 +1,12 @@
 /** forward declarations of STL data types
  */
-#ifndef SEM_CORE_STL_TYPEDEFS_FWD_H_
-#define SEM_CORE_STL_TYPEDEFS_FWD_H_
+#ifndef SEM_CORE_STL_TYPEDEFS_H_
+#define SEM_CORE_STL_TYPEDEFS_H_
 
 #include <string>
 #include <vector>
 
 typedef unsigned char uchar;
-
-//namespace std {
-
-//class string;
-
-//template <typename T> class vector;
-
-//} // fake namespace STL for fwd declaration
 
 typedef std::vector< std::string > VecS;    ///< Convinience typedef for vector of strings
 typedef std::vector< float > VecF;          ///< Convinience typedef for vector of floats
