c1ed1a6dc060b1bbeecf0c630ca4bb234a846b33
Youssef Kashef <youssef.kashef@gmail.com>
2015-01-14 13:34:51 +0100

    forward OpenCV's 3d point typedefs

--- modules/core/cv/typedefs_fwd.h
+++ modules/core/cv/typedefs_fwd.h
@@ -1,23 +1,34 @@
-/** forward declarations of boost data types
+/** forward typedef of OpenCV data types
  */
 #ifndef SEM_CORE_OPENCV_TYPEDEFS_FWD_H_
 #define SEM_CORE_OPENCV_TYPEDEFS_FWD_H_
 
 namespace cv {
 
-// only types that are defined by OpenCV
+// only typedefs already provided by OpenCV
 class Mat;
 
 template <typename T> class Mat_;
 
-typedef Mat_<float> Mat1f;   ///< convinience typedef for Mat of floats without constraints on no. of channels
-typedef Mat_<int> Mat1i;   ///< convinience typedef for Mat of integers
+typedef Mat_<float> Mat1f;  ///< convinience typedef for Mat of floats without constraints on no. of channels
+typedef Mat_<int> Mat1i;    ///< convinience typedef for Mat of integers
 
-template <typename T> class Point_;
+template <typename T> class Point_; ///< convinience typedef for point template
+/**
+ * @typedef 2d points
+ */
+typedef Point_<int> Point2i;    ///< convinience typedef for 2d point of integers
+typedef Point_<float> Point2f;  ///< convinience typedef for 2d point of floats
+typedef Point2i Point;          ///< extra-convinience typedef for 2d point of integers
 
-typedef Point_<int> Point2i;
-typedef Point_<float> Point2f;
-typedef Point2i Point;
+/
+template<typename _Tp> class Point3_;
+/**
+ * @typedef 3d points
+ */
+typedef Point3_<int> Point3i;       ///< convinience typedef for 3d point of integers
+typedef Point3_<float> Point3f;     ///< convinience typedef for 3d point of floats
+typedef Point3_<double> Point3d;    ///< convinience typedef for 3d point of doubles - not preferred
 
 } // fake namespace cv for fwd declaration
 
