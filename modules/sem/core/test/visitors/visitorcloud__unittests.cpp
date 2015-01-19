47ed16433c291b4ecbb60a70f0fa05725cffb973
Youssef Kashef <youssef.kashef@gmail.com>
2015-01-19 12:31:12 +0100

    rewrite visitor cloud tests as a typed test to cover point types

--- modules/sem/core/test/visitors/visitorcloud__unittests.cpp
+++ modules/sem/core/test/visitors/visitorcloud__unittests.cpp
@@ -6,151 +6,185 @@
 #include "sem/core/exception.h"
 #include "sem/core/pcl/vertices.h"
 #include "sem/ts/ts.h"
+#include "sem/ts/pcl_point_typed_tests.h"
 
 using namespace std;
 using namespace pcl;
 using namespace cv;
 using namespace sem;
+using namespace sem::ts;
 
 namespace {
 
 /**
- * @brief test class around VisitorCloud
+ * @brief test class around VisitorCloud_ with different point types
  */
-class VisitorCloudTest : public ::testing::Test
+template <class TPoint>
+class VisitorCloud_PointTypedTest : public ::testing::Test
 {
 protected:
     virtual void SetUp()
     {
-        to_.Reset();
+        this->to_.Reset();
+
+        this->m_ = Mat1f(4, PCLPointTraits_<TPoint >::FieldCount());
+        randn(this->m_, 0.f, 100.f);
     }
 
-    VisitorCloud_<PointXYZ> to_;    ///< test object
+    VisitorCloud_<TPoint > to_;     ///< test object
+    Mat1f m_;                       ///< fake data
 };
+TYPED_TEST_CASE(VisitorCloud_PointTypedTest, PCLPointTypes);
 
-TEST_F(VisitorCloudTest, Empty)
+TYPED_TEST(VisitorCloud_PointTypedTest, Empty)
 {
-    EXPECT_TRUE(to_(Mat_f())->empty());
-    EXPECT_TRUE(to_(Mat_f(0, 0))->empty());
-    EXPECT_TRUE(to_(Mat_f(1, 0))->empty());
-    EXPECT_TRUE(to_(Mat_f(0, 1))->empty());
+    typedef boost::shared_ptr<PointCloud<TypeParam > > CloudTPPtr;
 
-    CloudXYZPtr cld(new CloudXYZ);
-    EXPECT_TRUE(to_(cld)->empty());
+    EXPECT_TRUE(this->to_(Mat_f())->empty());
+    EXPECT_TRUE(this->to_(Mat_f(0, 0))->empty());
+    EXPECT_TRUE(this->to_(Mat_f(1, 0))->empty());
+    EXPECT_TRUE(this->to_(Mat_f(0, 1))->empty());
+
+    CloudTPPtr cld(new PointCloud<TypeParam >());
+    EXPECT_TRUE(this->to_(cld)->empty());
 }
 
-TEST_F(VisitorCloudTest, EmptySize)
+TYPED_TEST(VisitorCloud_PointTypedTest, EmptySize)
 {
-    EXPECT_EQ(size_t(0), to_(Mat_f())->size());
-    EXPECT_EQ(size_t(0), to_(Mat_f(0, 0))->size());
-    EXPECT_EQ(size_t(0), to_(Mat_f(1, 0))->size());
-    EXPECT_EQ(size_t(0), to_(Mat_f(0, 1))->size());
+    typedef boost::shared_ptr<PointCloud<TypeParam > > CloudTPPtr;
 
-    CloudXYZPtr cld(new CloudXYZ);
-    EXPECT_EQ(size_t(0), to_(cld)->size());
+    EXPECT_EQ(size_t(0), this->to_(Mat_f())->size());
+    EXPECT_EQ(size_t(0), this->to_(Mat_f(0, 0))->size());
+    EXPECT_EQ(size_t(0), this->to_(Mat_f(1, 0))->size());
+    EXPECT_EQ(size_t(0), this->to_(Mat_f(0, 1))->size());
+
+    CloudTPPtr cld(new PointCloud<TypeParam >());
+    EXPECT_EQ(size_t(0), this->to_(cld)->size());
 }
 
-TEST_F(VisitorCloudTest, FromMat_f)
+TYPED_TEST(VisitorCloud_PointTypedTest, FromMat_f)
 {
-    Mat1f m(4, 3);
-    randn(m, 0.f, 100.f);
+    typedef boost::shared_ptr<PointCloud<TypeParam > > CloudTPPtr;
 
-    CloudXYZPtr cld = to_(m);
-    Mat1f m2 = PointCloud2Mat_<PointXYZ>(cld);
-    hconcat(m, Mat1f(m.rows, 1, 1), m);
+    CloudTPPtr cld = this->to_(this->m_);
+    Mat1f m2 = PointCloud2Mat_<TypeParam >(cld);
 
-    EXPECT_MAT_EQ(m, m2);
-    EXPECT_NE(m.data, m2.data) << "Expecting deep copy. If intentionally optimized to be a shared copy, please update test.";
+    int padding = static_cast<int>(PCLPointTraits_<TypeParam>::NbFloats()-PCLPointTraits_<TypeParam>::FieldCount());
+    EXPECT_MAT_DIMS_EQ(m2, Size2i(this->m_.cols+padding, this->m_.rows));
+
+    EXPECT_MAT_EQ(this->m_, m2.colRange(0, this->m_.cols));
+    EXPECT_NE(this->m_.data, m2.data) << "Expecting deep copy. If intentionally optimized to be a shared copy, please update test.";
 }
 
-TEST_F(VisitorCloudTest, Cloud)
+TYPED_TEST(VisitorCloud_PointTypedTest, Cloud)
 {
-    Mat1f m(4, 3);
-    randn(m, 0.f, 100.f);
-    CloudXYZPtr cld = Mat2PointCloud_<PointXYZ>(m);
+    typedef boost::shared_ptr<PointCloud<TypeParam > > CloudTPPtr;
 
-    CloudXYZPtr cld2 = to_(cld);
+    CloudTPPtr cld = Mat2PointCloud_<TypeParam >(this->m_);
+
+    CloudTPPtr cld2 = this->to_(cld);
 
     EXPECT_EQ(cld->size(), cld2->size()) << "Size mismatch.";
     EXPECT_EQ(cld->width, cld2->width) << "Width mismatch.";
     EXPECT_EQ(cld->height, cld2->height) << "Height mismatch.";
 
-    for(CloudXYZ::iterator itr1=cld->begin(), itr2=cld2->begin();
+    for(typename PointCloud<TypeParam >::iterator itr1=cld->begin(), itr2=cld2->begin();
         itr1 != cld->end(); ++itr1, ++itr2) {
 
-        pcl::PointXYZ _p1 = *itr1;
-        pcl::PointXYZ _p2 = *itr2;
-        EXPECT_FLOAT_EQ(_p1.x, _p2.x) << "Unexpected value for x coordinate.";
-        EXPECT_FLOAT_EQ(_p1.y, _p2.y) << "Unexpected value for y coordinate.";
-        EXPECT_FLOAT_EQ(_p1.z, _p2.z) << "Unexpected value for z coordinate.";
+        TypeParam _p1 = *itr1;
+        TypeParam _p2 = *itr2;
+
+        float *pt_data_ptr1 = reinterpret_cast<float*>(&_p1);
+        float *pt_data_ptr2 = reinterpret_cast<float*>(&_p2);
+
+        EXPECT_NE(pt_data_ptr1, pt_data_ptr2) << "Pointers are pointing to the same memory location. Is this intentional?";
+
+        for(size_t i=0; i<PCLPointTraits_<TypeParam >::FieldCount(); i++) {
+
+            EXPECT_FLOAT_EQ(pt_data_ptr1[i], pt_data_ptr2[i]) << "Unexpected value for coordinate i=" << i;
+        }
     }
 }
 
-TEST_F(VisitorCloudTest, ResetValid)
+TYPED_TEST(VisitorCloud_PointTypedTest, ResetValid)
 {
-    Mat1f m(4, 3);
-    randn(m, 0.f, 100.f);
+    typedef boost::shared_ptr<PointCloud<TypeParam > > CloudTPPtr;
 
-    CloudXYZPtr cld1 = to_(m);
-    EXPECT_NO_THROW(to_.Reset());
+    CloudTPPtr cld1 = this->to_(this->m_);
+    EXPECT_NO_THROW(this->to_.Reset());
 
-    CloudXYZPtr cld2 = Mat2PointCloud_<PointXYZ>(m);
-    CloudXYZPtr cld3 = to_(cld2);
-    EXPECT_NO_THROW(to_.Reset());
+    CloudTPPtr cld2 = Mat2PointCloud_<TypeParam >(this->m_);
+    CloudTPPtr cld3 = this->to_(cld2);
+    EXPECT_NO_THROW(this->to_.Reset());
 
-    cld1 = to_(m);
-    EXPECT_NO_THROW(to_.Reset());
+    cld1 = this->to_(this->m_);
+    EXPECT_NO_THROW(this->to_.Reset());
 }
 
 /**
- * @brief Thest this visitor's caching
+ * @brief Test this visitor's caching
  */
-TEST_F(VisitorCloudTest, Reset)
+TYPED_TEST(VisitorCloud_PointTypedTest, Reset)
 {
-    Mat1f m(4, 3, 1.f);
-    Mat1f m0 = m.clone();
-    hconcat(m0, Mat1f(m0.rows, 1, 1), m0);
+    typedef boost::shared_ptr<PointCloud<TypeParam > > CloudTPPtr;
 
-    CloudXYZPtr cld = to_(m);
-    EXPECT_MAT_EQ(PointCloud2Mat_<PointXYZ>(cld), m0);
+    this->m_.setTo(1.f);
+    Mat1f m0 = this->m_.clone();
 
-    cld = to_(m); // result of first call is cached.
+    CloudTPPtr cld = this->to_(this->m_);
+
+    Mat1f mat_cld = PointCloud2Mat_<TypeParam >(cld);
+
+    int padding = static_cast<int>(PCLPointTraits_<TypeParam>::NbFloats()-PCLPointTraits_<TypeParam>::FieldCount());
+    EXPECT_MAT_DIMS_EQ(mat_cld, Size2i(m0.cols+padding, m0.rows));
+    EXPECT_MAT_EQ(m0, mat_cld.colRange(0, m0.cols)) << "Not using cahced cloud.";
+
+    cld = this->to_(this->m_); // result of first call is cached.
 
     const int N=2;
     ASSERT_GT(N, 1) << "This test must iterate at least twice to see effect of Reset() on cached reference.";
     for(int i=0; i<2; i++) {
 
-        cld = to_(m+1);
+        cld = this->to_(this->m_+1);
 
         if(i == 0) {
 
-            EXPECT_MAT_EQ(PointCloud2Mat_<PointXYZ>(cld), m0) << "Not suing cahced cloud.";
+            mat_cld = PointCloud2Mat_<TypeParam >(cld);
+            EXPECT_MAT_DIMS_EQ(mat_cld, Size2i(m0.cols+padding, m0.rows));
+            EXPECT_MAT_EQ(m0, mat_cld.colRange(0, m0.cols)) << "Not using cahced cloud.";
         }
         else {
 
             Mat1f m2 = m0.clone();
-            m2.colRange(0, m2.cols-1).setTo(2.f);
-            EXPECT_MAT_EQ(PointCloud2Mat_<PointXYZ>(cld), m2) << "Still using cached cloud";
+            m2.colRange(0, m2.cols).setTo(2.f);
+
+            mat_cld = PointCloud2Mat_<TypeParam >(cld);
+            EXPECT_MAT_DIMS_EQ(mat_cld, Size2i(m2.cols+padding, m2.rows));
+            EXPECT_MAT_EQ(m2, mat_cld.colRange(0, m2.cols)) << "Still using cached cloud";
         }
-        to_.Reset();
+        this->to_.Reset();
     }
 }
 
-TEST_F(VisitorCloudTest, FromVecVertices)
+TYPED_TEST(VisitorCloud_PointTypedTest, FromVecVertices)
 {
-    Mat1f m(4, 3);
-    for(size_t i=0; i<m.total(); i++) {
+    typedef boost::shared_ptr<PointCloud<TypeParam > > CloudTPPtr;
 
-        m(i) = static_cast<float>(randu<uint32_t>() % 256);
+    for(size_t i=0; i<this->m_.total(); i++) {
+
+        this->m_(i) = static_cast<float>(randu<uint32_t>() % 256);
     }
 
-    CloudXYZPtr cld = to_(Mat2VecVertices(m));
+    CloudTPPtr cld = this->to_(Mat2VecVertices(this->m_));
 
-    Mat1f m2 = PointCloud2Mat_<PointXYZ>(cld);
-    hconcat(m, Mat1f(m.rows, 1, 1), m);
+    Mat1f mat_cld = PointCloud2Mat_<TypeParam >(cld);
 
-    EXPECT_MAT_EQ(m, m2);
-    EXPECT_NE(m.data, m2.data) << "Expecting deep copy. If intentionally optimized to be a shared copy, please update test.";
+    int padding = static_cast<int>(PCLPointTraits_<TypeParam>::NbFloats()-PCLPointTraits_<TypeParam>::FieldCount());
+
+    EXPECT_MAT_DIMS_EQ(mat_cld, Size2i(this->m_.cols+padding, this->m_.rows));
+    EXPECT_MAT_EQ(this->m_, mat_cld.colRange(0, this->m_.cols));
+    EXPECT_NE(this->m_.data, mat_cld.data) << "Expecting deep copy. " <<
+                                              "If intentionally optimized to be a shared copy, please update test.";
 }
 
 } // annonymous namespace for cloud visitors' test fixtures
