def44561551ae43aab60312de4f7e850cf9b76b3
Youssef Kashef <youssef.kashef@gmail.com>
2015-01-22 16:55:39 +0100

    add sample project placeholder

---
+++ samples/dummy_sample/sample.cpp
@@ -0,0 +1,14 @@
+/** @file Dummy sample main
+ */
+#include <iostream>
+
+#include "elm/core/core.h"
+
+using namespace std;
+
+int main() {
+
+    cout<<elm::GetVersion()<<endl;
+
+    return 0;
+}
