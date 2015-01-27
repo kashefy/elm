7adb02f1f367bd9e43de42dcaa33d9d866c067ed
Youssef Kashef <youssef.kashef@gmail.com>
2015-01-27 16:08:25 +0100

    start implement graduated assignment layer

--- modules/elm/layers/gradassignment.cpp
+++ modules/elm/layers/gradassignment.cpp
@@ -1,8 +1,73 @@
 #include "elm/layers/gradassignment.h"
 
+#include "elm/core/layerconfig.h"
+#include "elm/core/signal.h"
+
+using namespace cv;
 using namespace elm;
+
+GradAssignment::~GradAssignment()
+{
+}
 
 GradAssignment::GradAssignment()
     : base_Layer()
 {
+    Clear();
+}
+
+GradAssignment::GradAssignment(const LayerConfig &cfg)
+    : base_Layer(cfg)
+{
+    Reset(cfg);
+}
+
+void GradAssignment::Clear()
+{
+    m_ = Mat1f();
+}
+
+void GradAssignment::Reset(const LayerConfig &config)
+{
+    Clear();
+    Reconfigure(config);
+}
+
+void GradAssignment::Reconfigure(const LayerConfig &config)
+{
+    PTree params = config.Params();
+
+    beta_0_ = params.get<float>(PARAM_BETA);
+    if(beta_0_ <= 0.f) {
+
+        ELM_THROW_VALUE_ERROR("Control parameter beta must be positive.");
+    }
+
+    beta_max_ = params.get<float>(PARAM_BETA_MAX);
+
+    beta_rate_ = params.get<float>(PARAM_BETA_RATE);
+    if(beta_rate_ <= 1.f) {
+
+        ELM_THROW_VALUE_ERROR("rate must be > 1.");
+    }
+
+    max_iter_per_beta_ = params.get<int>(PARAM_MAX_ITER_SINKHORN);
+    max_iter_sinkhorn_ = params.get<int>(PARAM_MAX_ITER_PER_BETA);
+}
+
+void GradAssignment::IONames(const LayerIONames &config)
+{
+    name_g_ab_ = config.Input(KEY_INPUT_GRAPH_AB);
+    name_g_ij_ = config.Input(KEY_INPUT_GRAPH_IJ);
+    name_out_m_ = config.Output(KEY_OUTPUT_M);
+}
+
+void GradAssignment::Activate(const Signal &signal)
+{
+
+}
+
+void GradAssignment::Response(Signal &signal)
+{
+    signal.Append(name_out_m_, m_);
 }
--- modules/elm/layers/gradassignment.h
+++ modules/elm/layers/gradassignment.h
@@ -1,14 +1,67 @@
 #ifndef ELM_LAYERS_GRADASSIGNMENT_H_
 #define ELM_LAYERS_GRADASSIGNMENT_H_
 
+#include <string>
+
+#include <opencv2/core.hpp>
+
 #include "elm/core/base_Layer.h"
+
+class LayerConfig;
+class Signal;
 
 namespace elm {
 
+/**
+ * @brief Layer for implementing Graduated Assignment algorithm for graph matching
+ * @cite Gold1996
+ */
 class GradAssignment : public base_Layer
 {
 public:
+    // params
+    static const std::string PARAM_BETA;        ///< initial value of the control parameter (deterministic annealing)
+    static const std::string PARAM_BETA_MAX;    ///< max. value of the control parameter
+    static const std::string PARAM_BETA_RATE;   ///< rate at which control param. increases
+    static const std::string PARAM_MAX_ITER_PER_BETA; ///< max. no. of iterations allowed per beta value
+    static const std::string PARAM_MAX_ITER_SINKHORN; ///< max. no. of iterations allowed for Sinkhorn's balancing method
+
+    // I/O Names
+    static const std::string KEY_INPUT_GRAPH_AB; ///< key to graph ab's adjacency matrix
+    static const std::string KEY_INPUT_GRAPH_IJ; ///< key to graph ij's adjacency matrix
+    static const std::string KEY_OUTPUT_M;       ///< match matrix variables
+
+    virtual ~GradAssignment();
+
     GradAssignment();
+
+    GradAssignment(const LayerConfig &cfg);
+
+    virtual void Clear();
+
+    virtual void Reset(const LayerConfig &config);
+
+    virtual void Reconfigure(const LayerConfig &config);
+
+    virtual void IONames(const LayerIONames &config);
+
+    virtual void Activate(const Signal &signal);
+
+    virtual void Response(Signal &signal);
+
+protected:
+    std::string name_g_ab_;     ///< name of graph ab adj. matrix in signal object
+    std::string name_g_ij_;     ///< name of graph ij adj. matrix in signal object
+    std::string name_out_m_;    ///< desintation name for match matrix
+
+    float beta_0_;      ///< initial value for control param. beta
+    float beta_max_;    ///< max. value for control param.
+    float beta_rate_;   ///< rate at which control param. increases
+
+    int max_iter_per_beta_; ///< max. no. of iterations allowed per beta value
+    int max_iter_sinkhorn_; ///< max. no. of iterations allowed for Sinkhorn's balancing method
+
+    cv::Mat1f m_;       ///< match matrix variables
 };
 
 } // namespace elm
