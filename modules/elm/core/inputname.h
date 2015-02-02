2385ab72fe417a4c8d13192f41006e5dff842bc0
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-02 12:35:03 +0100

    add overloaded constructor signatures

--- modules/elm/core/inputname.h
+++ modules/elm/core/inputname.h
@@ -1,10 +1,20 @@
 #ifndef _ELM_CORE_INPUTNAME_H_
 #define _ELM_CORE_INPUTNAME_H_
 
+#include <string>
+
+#include <boost/property_tree/ptree.hpp>
+
+namespace elm {
+
 class InputName
 {
 public:
-    InputName();
+    InputName(const std::string &name);
+
+    InputName(const boost::property_tree::ptree &p);
 };
+
+} // namespace elm
 
 #endif // _ELM_CORE_INPUTNAME_H_
