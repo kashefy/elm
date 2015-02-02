7cbffe6960a6881570615e1ff2001b7caae91bad
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-02 17:26:10 +0100

    placeholder for time-invariant base layer

---
+++ modules/elm/layers/base_layer_derivations/base_tivlayer.cpp
@@ -0,0 +1,12 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, Elm Library Project
+// 3-clause BSD License
+//
+//M*/
+#include "elm/layers/base_layer_derivations/base_tivlayer.h"
+
+base_TIVLayer::base_TIVLayer()
+{
+}
---
+++ modules/elm/layers/base_layer_derivations/base_tivlayer.h
@@ -0,0 +1,31 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, Elm Library Project
+// 3-clause BSD License
+//
+//M*/
+#ifndef _ELM_CORE_BASE_LAYER_DERIVS_BASE_TIVLAYER_H_
+#define _ELM_CORE_BASE_LAYER_DERIVS_BASE_TIVLAYER_H_
+
+#include "elm/core/base_Layer.h"
+
+namespace elm {
+
+/**
+ * @brief class for time-invariant layer
+ */
+class base_TIVLayer : public base_Layer
+{
+public:
+    virtual ~base_TIVLayer();
+
+protected:
+    base_TIVLayer();
+
+    base_TIVLayer(const LayerConfig& cfg);
+};
+
+} // namespace elm
+
+#endif // _ELM_CORE_BASE_LAYER_DERIVS_BASE_TIVLAYER_H_
