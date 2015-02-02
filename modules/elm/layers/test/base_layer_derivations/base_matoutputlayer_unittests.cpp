e69ba4195506db24aee9db418b48a58ecbb8d7ef
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-02 18:28:46 +0100

    add more tests around DummyMatOutputLayer

--- modules/elm/layers/test/base_layer_derivations/base_matoutputlayer_unittests.cpp
+++ modules/elm/layers/test/base_layer_derivations/base_matoutputlayer_unittests.cpp
@@ -13,6 +13,7 @@
 
 #include "elm/core/layerionames.h"
 #include "elm/core/signal.h"
+#include "elm/ts/layer_assertions.h"
 
 using namespace std;
 using namespace cv;
@@ -42,7 +43,7 @@ public:
 
     virtual void Activate(const Signal &signal) {
 
-        m_ = signal.MostRecentMat(name_in_);
+        m_ = signal.MostRecentMat(name_in_)*2.f;
     }
 
     DummyMatOutputLayer() {}
@@ -58,15 +59,40 @@ protected:
     virtual void SetUp()
     {
         to_.reset(new DummyMatOutputLayer());
+
+        LayerIONames io;
+        io.Input(DummyMatOutputLayer::KEY_INPUT_M, NAME_IN_M);
+        io.Output(DummyMatOutputLayer::KEY_OUTPUT_M, NAME_OUT_M);
+
+        to_->IONames(io);
+
+        sig_.Append(NAME_IN_M, Mat1f(3, 4, 1.f));
+
     }
 
+    virtual void TearDown()
+    {
+        sig_.Clear();
+    }
+
+    // members:
     shared_ptr<base_Layer> to_; ///< test object
+    Signal sig_;
 };
 
-TEST_F(MatOutputLayerTest, Test)
+TEST_F(MatOutputLayerTest, Sanity)
 {
-    cout<<base_MatOutputLayer::KEY_OUTPUT_M<<std::endl;
-    cout<<DummyMatOutputLayer::KEY_OUTPUT_M<<std::endl;
+    EXPECT_EQ(base_MatOutputLayer::KEY_OUTPUT_M,
+              DummyMatOutputLayer::KEY_OUTPUT_M);
+}
+
+TEST_F(MatOutputLayerTest, ActivateAndResponse)
+{
+    to_->Activate(sig_);
+    to_->Response(sig_);
+
+    EXPECT_MAT_EQ(sig_.MostRecentMat(NAME_IN_M)*2,
+                  sig_.MostRecentMat(NAME_OUT_M));
 }
 
 } // annonymous namespace for test cases and test fixtures
