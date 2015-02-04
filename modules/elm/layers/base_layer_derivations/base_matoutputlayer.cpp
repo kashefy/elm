43701519361add6f865b53af24613648c857b35e
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-04 15:53:43 +0100

    add license header

--- modules/elm/layers/base_layer_derivations/base_matoutputlayer.cpp
+++ modules/elm/layers/base_layer_derivations/base_matoutputlayer.cpp
@@ -1,3 +1,10 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
 #include "elm/layers/base_layer_derivations/base_matoutputlayer.h"
 
 #include "elm/core/layerionames.h"
