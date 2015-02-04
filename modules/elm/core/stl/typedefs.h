f5547fc61e9023d7ee1e4dc3479be1525bf1afa7
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-04 18:19:32 +0100

    pull typedefs into elm namespace

--- modules/elm/core/stl/typedefs.h
+++ modules/elm/core/stl/typedefs.h
@@ -14,10 +14,14 @@
 #include <string>
 #include <vector>
 
+namespace elm {
+
 typedef unsigned char uchar;
 
 typedef std::vector< std::string > VecS;    ///< Convinience typedef for vector of strings
 typedef std::vector< float > VecF;          ///< Convinience typedef for vector of floats
 typedef std::map<std::string, std::string> MapSS;   ///< Convinience typedef for map of string keys and string values
+
+}
 
 #endif // _ELM_CORE_STL_TYPEDEFS_FWD_H_
