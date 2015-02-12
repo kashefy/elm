0fbe52e663800338c3e8a88c6d06c6a69a963b4a
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-12 13:13:53 +0100

    visitor for converting types to SparseMat1f

---
+++ modules/elm/core/visitors/visitorsparsemat1f.cpp
@@ -0,0 +1,80 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#include "elm/core/visitors/visitorsparsemat1f.h"
+
+#include <opencv2/core.hpp>
+
+#include "elm/core/exception.h"
+#include "elm/core/pcl/cloud_.h"
+#include "elm/core/pcl/vertices.h"
+
+using namespace cv;
+using namespace elm;
+
+SparseMat1f VisitorSparseMat1f::operator()(const SparseMat1f &m) const
+{
+    return m;
+}
+
+SparseMat1f VisitorSparseMat1f::operator()(const Mat &m) const
+{
+    ELM_THROW_BAD_DIMS_IF(m.empty(), "Cannot create a sparse matrix from empty Mat");
+    if((m.type() & CV_MAT_DEPTH_MASK) != (CV_32F & CV_MAT_DEPTH_MASK)) {
+
+        Mat mf;
+        m.convertTo(mf, CV_MAKE_TYPE(CV_32F, m.channels()));
+        return mf;
+    }
+    else {
+        return m;
+    }
+}
+
+SparseMat1f VisitorSparseMat1f::operator()(const Mat1f &m) const
+{
+    return (m.empty())? SparseMat1f() : SparseMat1f(m);
+}
+
+SparseMat1f VisitorSparseMat1f::operator()(float f) const
+{
+    return SparseMat1f(Mat1f(1, 1, f));
+}
+
+SparseMat1f VisitorSparseMat1f::operator()(int n) const
+{
+    return SparseMat1f(Mat1f(1, 1, static_cast<float>(n)));
+}
+
+SparseMat1f VisitorSparseMat1f::operator()(uchar c) const
+{
+    return SparseMat1f(Mat1f(1, 1, static_cast<float>(c)));
+}
+
+#ifdef __WITH_PCL // definitions below require PCL support
+
+SparseMat1f VisitorSparseMat1f::operator()(const VecVertices &vv) const
+{
+    return this->operator ()(VecVertices2Mat(vv, false));
+}
+
+SparseMat1f VisitorSparseMat1f::operator()(CloudXYZPtr &c) const
+{
+    return this->operator ()(PointCloud2Mat_<pcl::PointXYZ>(c));
+}
+
+SparseMat1f VisitorSparseMat1f::operator()(CloudNrmlPtr &c) const
+{
+    return this->operator ()(PointCloud2Mat_<pcl::Normal>(c));
+}
+
+SparseMat1f VisitorSparseMat1f::operator()(CloudPtNrmlPtr &c) const
+{
+    return this->operator ()(PointCloud2Mat_<pcl::PointNormal>(c));
+}
+
+#endif // __WITH_PCL
---
+++ modules/elm/core/visitors/visitorsparsemat1f.h
@@ -0,0 +1,61 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+/**
+ * @file Define variant visitor classes around SparseMat1f <-> X type conversions
+ */
+#ifndef _ELM_CORE_VISITORSPARSEMAT1f_H_
+#define _ELM_CORE_VISITORSPARSEMAT1f_H_
+
+#include "elm/core/typedefs_fwd.h"
+#include "elm/core/visitors/visitor_.h"
+
+#include "elm/core/pcl/typedefs_fwd.h"
+
+namespace elm {
+
+/**
+ * @brief visitor class for converting to a sparse Mat of floats
+ */
+class VisitorSparseMat1f :
+        public Visitor_<elm::SparseMat1f >
+{
+public:
+    elm::SparseMat1f operator()(const elm::SparseMat1f &m) const;
+
+    /**
+     * @throws elm::ExceptionBadDims on empty input
+     */
+    elm::SparseMat1f operator()(const cv::Mat &m) const;
+
+    /**
+     * @throws elm::ExceptionBadDims on empty input
+     */
+    elm::SparseMat1f operator()(const cv::Mat1f &m) const;
+
+    elm::SparseMat1f operator()(float f) const;
+
+    elm::SparseMat1f operator()(int n) const;
+
+    elm::SparseMat1f operator()(uchar c) const;
+
+#ifdef __WITH_PCL // this conversion requires PCL support
+
+    elm::SparseMat1f operator()(elm::CloudXYZPtr &c) const;
+
+    elm::SparseMat1f operator()(elm::CloudNrmlPtr &c) const;
+
+    elm::SparseMat1f operator()(elm::CloudPtNrmlPtr &c) const;
+
+    elm::SparseMat1f operator()(const elm::VecVertices &vv) const;
+
+#endif // __WITH_PCL
+};
+
+} // namespace elm
+
+#endif // _ELM_CORE_VISITORSPARSEMAT1f_H_
