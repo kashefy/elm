62a33a5aa6a30e9f8a0444a9325edcc1d88d6aab
Youssef Kashef <youssef.kashef@gmail.com>
2015-03-18 18:15:57 +0100

    initial definitions for MLP layer

--- modules/elm/layers/mlp.cpp
+++ modules/elm/layers/mlp.cpp
@@ -1,7 +1,44 @@
 #include "elm/layers/mlp.h"
 
-//using namespace elm;
+using namespace elm;
+
+MLP::~MLP()
+{
+}
 
 MLP::MLP()
+    : base_SupervisedBatch()
 {
+    Clear();
+}
+
+MLP::MLP(const LayerConfig &cfg)
+    : base_SupervisedBatch(cfg)
+{
+    Reset(cfg);
+}
+
+void MLP::Clear()
+{
+
+}
+
+void MLP::Reset(const LayerConfig &config)
+{
+    Reconfigure(config);
+}
+
+void MLP::Reconfigure(const LayerConfig &config)
+{
+
+}
+
+void MLP::Activate(const Signal &signal)
+{
+
+}
+
+void MLP::Learn(const cv::Mat1f &features, const cv::Mat1f &labels)
+{
+
 }
--- modules/elm/layers/mlp.h
+++ modules/elm/layers/mlp.h
@@ -1,10 +1,33 @@
-#ifndef MLP_H
-#define MLP_H
+#ifndef _ELM_LAYERS_MLP_H_
+#define _ELM_LAYERS_MLP_H_
 
-class MLP
+#include "elm/layers/layers_interim/learning/base_SupervisedBatch.h"
+
+namespace elm {
+
+/**
+ * @brief Layer class for mult-layer perceptrons
+ */
+class MLP : public base_SupervisedBatch
 {
 public:
+    ~MLP();
+
     MLP();
+
+    MLP(const LayerConfig &cfg);
+
+    void Clear();
+
+    void Reset(const LayerConfig &config);
+
+    void Reconfigure(const LayerConfig &config);
+
+    void Activate(const Signal &signal);
+
+    virtual void Learn(const cv::Mat1f &features, const cv::Mat1f &labels);
 };
 
-#endif // MLP_H
+} // namespace elm
+
+#endif // _ELM_LAYERS_MLP_H_
