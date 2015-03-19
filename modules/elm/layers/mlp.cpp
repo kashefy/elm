1a57b3f7444dd4d20c5801486fe36a7f6c43cdf7
Youssef Kashef <youssef.kashef@gmail.com>
2015-03-19 17:32:51 +0100

    flesh out mlp layer

--- modules/elm/layers/mlp.cpp
+++ modules/elm/layers/mlp.cpp
@@ -1,5 +1,12 @@
 #include "elm/layers/mlp.h"
 
+#include "elm/core/boost/translators/transl_str_cvtermcriteria.h"
+#include "elm/core/boost/translators/transl_str_veci.h"
+#include "elm/core/layerconfig.h"
+#include "elm/core/signal.h"
+
+using namespace std;
+using namespace cv;
 using namespace elm;
 
 MLP::~MLP()
@@ -20,25 +27,44 @@ MLP::MLP(const LayerConfig &cfg)
 
 void MLP::Clear()
 {
-
+    mlp_.clear();
 }
 
 void MLP::Reset(const LayerConfig &config)
 {
+    PTree p = config.Params();
+    VecI arch = p.get<VecI >(PARAM_ARCH);
+
+    const int NB_LAYERS = static_cast<int>(arch.size());
+
+    ELM_THROW_BAD_DIMS_IF(NB_LAYERS < 2, "Need at least 2 layers for mlp.");
+
+    Mat1i layers(arch);
+    layers = layers.reshape(1, NB_LAYERS); // resize to column matrix, row per layer
+
+    mlp_.create(layers);
+
     Reconfigure(config);
 }
 
 void MLP::Reconfigure(const LayerConfig &config)
 {
+    PTree p = config.Params();
 
+    params_.train_method    = p.get<int>(PARAM_TRAIN_METHOD, CvANN_MLP_TrainParams::BACKPROP);
+    params_.bp_dw_scale     = p.get<float>(PARAM_BP_DW_SCALE, 0.05f);
+    params_.bp_moment_scale = p.get<float>(PARAM_BP_MOMENT_SCALE, 0.05f);
+
+    params_.term_crit = p.get<CvTermCriteria>(PARAM_TERM_CRITERIA);
 }
 
 void MLP::Activate(const Signal &signal)
 {
-
+    Mat1f sample = signal.MostRecentMat1f(name_input_);
+    mlp_.predict(sample, m_);
 }
 
-void MLP::Learn(const cv::Mat1f &features, const cv::Mat1f &labels)
+void MLP::Learn(const Mat1f &features, const Mat1f &labels)
 {
-
+    mlp_.train(features, labels, Mat(), Mat(), params_);
 }
--- modules/elm/layers/mlp.h
+++ modules/elm/layers/mlp.h
@@ -3,6 +3,8 @@
 
 #include "elm/layers/layers_interim/learning/base_SupervisedBatch.h"
 
+#include <opencv2/ml/ml.hpp>
+
 namespace elm {
 
 /**
@@ -11,6 +13,12 @@ namespace elm {
 class MLP : public base_SupervisedBatch
 {
 public:
+    static const std::string PARAM_ARCH;                ///< network architecture (how many nodes per layer input,[hidden1, hidden2,...], output)
+    static const std::string PARAM_TERM_CRITERIA;       ///< termination criteria
+    static const std::string PARAM_TRAIN_METHOD;        ///< training method
+    static const std::string PARAM_BP_DW_SCALE;         ///< backpropagation(BP)'s delta weight scale
+    static const std::string PARAM_BP_MOMENT_SCALE;     ///< BP's moment scale
+
     ~MLP();
 
     MLP();
@@ -26,6 +34,12 @@ public:
     void Activate(const Signal &signal);
 
     virtual void Learn(const cv::Mat1f &features, const cv::Mat1f &labels);
+
+protected:
+
+    // members
+    CvANN_MLP mlp_;                 ///< neural network
+    CvANN_MLP_TrainParams params_;  ///< training params
 };
 
 } // namespace elm
