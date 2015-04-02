e90eeb7da94d03a225b5b8d2430ada797d52a664
Youssef Kashef <youssef.kashef@gmail.com>
2015-04-02 17:19:06 +0200

    map matlab class type to possible OpenCV Mat type

---
+++ modules/elm/io/matio_utils.cpp
@@ -0,0 +1,33 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#include "elm/io/matio_utils.h"
+
+#include <opencv2/core/core.hpp>
+
+#include "elm/core/exception.h"
+
+using namespace cv;
+
+unsigned int elm::MATIOClassTOCV_TYPE(matio_classes type)
+{
+    unsigned int cv_type;
+    switch(type) {
+
+    case MAT_C_DOUBLE:
+        cv_type = CV_64F;
+        break;
+    case MAT_C_UINT16:
+        cv_type = CV_16U;
+        break;
+
+    default:
+        ELM_THROW_TYPE_ERROR("unrecognized type");
+    }
+
+    return cv_type;
+}
---
+++ modules/elm/io/matio_utils.h
@@ -0,0 +1,20 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#ifndef _ELM_IO_MATIO_UTILS_H_
+#define _ELM_IO_MATIO_UTILS_H_
+
+#include "matio.h"
+
+namespace elm {
+
+unsigned int MATIOClassTOCV_TYPE(matio_classes type);
+
+
+} // namespace elm
+
+#endif // _ELM_IO_MATIO_UTILS_H_
