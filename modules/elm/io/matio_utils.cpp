07e94401315d73fcd0c617e664e44c570aee2218
Youssef Kashef <youssef.kashef@gmail.com>
2015-04-02 17:47:25 +0200

    add more type mappings

--- modules/elm/io/matio_utils.cpp
+++ modules/elm/io/matio_utils.cpp
@@ -20,11 +20,29 @@ unsigned int elm::MATIOClassTOCV_TYPE(matio_classes type)
     unsigned int cv_type;
     switch(type) {
 
+    case MAT_C_CHAR:
+        cv_type = CV_8S; // or unsigned?
+        break;
     case MAT_C_DOUBLE:
         cv_type = CV_64F;
         break;
+    case MAT_C_UINT8:
+        cv_type = CV_8U;
+        break;
     case MAT_C_UINT16:
         cv_type = CV_16U;
+        break;
+    case MAT_C_INT8:
+        cv_type = CV_8S;
+        break;
+    case MAT_C_INT16:
+        cv_type = CV_16S;
+        break;
+    case MAT_C_INT32:
+        cv_type = CV_32S;
+        break;
+    case MAT_C_SINGLE:
+        cv_type = CV_32F;
         break;
 
     default:
