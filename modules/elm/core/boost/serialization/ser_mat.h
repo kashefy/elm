86f2ee10b2bc35710f33af7d07bef1220be1430d
Youssef Kashef <youssef.kashef@gmail.com>
2015-04-09 18:25:00 +0200

    initial addition for OpenCV Mat serialization

---
+++ modules/elm/core/boost/serialization/ser_mat.h
@@ -0,0 +1,31 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#ifndef _ELM_CORE_BOOST_SERIALIZATION_SER_MAT_H_
+#define _ELM_CORE_BOOST_SERIALIZATION_SER_MAT_H_
+
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/nvp.hpp>
+
+#include <opencv2/core/core.hpp>
+
+namespace boost {
+namespace serialization {
+
+    template<class Tarchive>
+    void serialize(Tarchive &ar, cv::Mat &obj, const unsigned int version)
+    {
+        using namespace boost::serialization;
+//        ar & make_nvp("epsilon",    obj.epsilon);
+//        ar & make_nvp("max_iter",   obj.max_iter);
+//        ar & make_nvp("type",       obj.type);
+    }
+
+} // namespace boost
+} // namespace serialization
+
+#endif // _ELM_CORE_BOOST_SERIALIZATION_SER_MAT_H_
