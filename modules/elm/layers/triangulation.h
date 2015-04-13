82461a2f66ef23ecf69996bbf3a51e378aeee4bb
Youssef Kashef <youssef.kashef@gmail.com>
2015-04-13 23:13:52 +0200

    keep triangulation layer definition within elm namespace even if not supported

--- modules/elm/layers/triangulation.h
+++ modules/elm/layers/triangulation.h
@@ -11,7 +11,9 @@
 #ifndef __WITH_PCL
     #warning "Disabling Triangulation layer due to no PCL support and defining it as a non-supported"
     #include "elm/layers/layernotsupported.h"
+namespace elm {
     class Triangulation : public ELM_LAYER_NOT_SUPPORTED(Triangulation, "Building with PCL is required for supporting Triangulation layer.");
+} // namespace elm
 #else   // __WITH_PCL
 
 #include "boost/optional.hpp"
