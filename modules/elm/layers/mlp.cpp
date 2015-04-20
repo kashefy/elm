91ab30162588977877fbeecf335e3352ffbb5f8b
Youssef Kashef <youssef.kashef@gmail.com>
2015-04-20 16:04:08 +0200

    add missing license header

--- modules/elm/layers/mlp.cpp
+++ modules/elm/layers/mlp.cpp
@@ -1,3 +1,10 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
 #include "elm/layers/mlp.h"
 
 #include "elm/core/boost/translators/transl_str_cvtermcriteria.h"
