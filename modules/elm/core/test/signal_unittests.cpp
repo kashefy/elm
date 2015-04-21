0b2cd95796ff1a967e8956ca0b0974d5e7df4e54
Youssef Kashef <youssef.kashef@gmail.com>
2015-04-21 17:20:57 +0200

    add test coverage around Append() overload

--- modules/elm/core/test/signal_unittests.cpp
+++ modules/elm/core/test/signal_unittests.cpp
@@ -61,7 +61,7 @@ TEST_F(SignalTest, GetFeatures)
     EXPECT_MAT_EQ(to_["bar"][0], Mat1f());
 }
 
-TEST_F(SignalTest, MostRecentMat)
+TEST_F(SignalTest, MostRecentMat1f)
 {
     EXPECT_THROW(to_.MostRecentMat1f("wrong"), ExceptionKeyError);
     EXPECT_THROW(to_.MostRecentMat1f("Foo"), ExceptionKeyError);
@@ -76,4 +76,126 @@ TEST_F(SignalTest, MostRecentMat)
     }
 }
 
+TEST_F(SignalTest, AppendMat1f)
+{
+    Signal to; ///< test object
 
+    to.Append("foo", Mat1f(3, 2, 1.f));
+    EXPECT_SIZE(1, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 2, 1.f), to.MostRecentMat1f("foo"));
+
+    to.Append("foo", Mat1f(3, 2, 2.f));
+    EXPECT_SIZE(1, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 2, 2.f), to.MostRecentMat1f("foo"));
+
+    to.Append("bar", Mat1f(3, 2, 3.f));
+    EXPECT_SIZE(2, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 2, 3.f), to.MostRecentMat1f("bar"));
+    EXPECT_MAT_EQ(Mat1f(3, 2, 2.f), to.MostRecentMat1f("foo"));
+}
+
+TEST_F(SignalTest, AppendFeatureData)
+{
+    Signal to; ///< test object
+
+    to.Append("foo", FeatureData(Mat1f(3, 2, 1.f)));
+    EXPECT_SIZE(1, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 2, 1.f), to.MostRecentMat1f("foo"));
+
+    to.Append("foo", FeatureData(Mat1f(3, 2, 2.f)));
+    EXPECT_SIZE(1, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 2, 2.f), to.MostRecentMat1f("foo"));
+
+    to.Append("bar", FeatureData(Mat1f(3, 2, 3.f)));
+    EXPECT_SIZE(2, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 2, 3.f), to.MostRecentMat1f("bar"));
+    EXPECT_MAT_EQ(Mat1f(3, 2, 2.f), to.MostRecentMat1f("foo"));
+}
+
+TEST_F(SignalTest, AppendSparseMat1f)
+{
+    Signal to; ///< test object
+
+    to.Append("foo", SparseMat1f(Mat1f(3, 2, 1.f)));
+    EXPECT_SIZE(1, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 2, 1.f), to.MostRecentMat1f("foo"));
+
+    to.Append("foo", SparseMat1f(Mat1f(3, 2, 2.f)));
+    EXPECT_SIZE(1, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 2, 2.f), to.MostRecentMat1f("foo"));
+
+    to.Append("bar", SparseMat1f(Mat1f(3, 2, 3.f)));
+    EXPECT_SIZE(2, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 2, 3.f), to.MostRecentMat1f("bar"));
+    EXPECT_MAT_EQ(Mat1f(3, 2, 2.f), to.MostRecentMat1f("foo"));
+}
+
+TEST_F(SignalTest, AppendVecMat1f)
+{
+    Signal to; ///< test object
+
+    {
+        VecMat1f v;
+        v.push_back(Mat1f(1, 1, 1.f));
+        to.Append("foo", v);
+        EXPECT_SIZE(1, to.FeatureNames());
+
+        VecMat1f v2 = to.MostRecent("foo").get<VecMat1f>();
+        EXPECT_SIZE(1, v2);
+        EXPECT_MAT_EQ(Mat1f(1, 1, 1.f), v2[0]);
+    }
+
+    {
+        VecMat1f v;
+        v.push_back(Mat1f(2, 1, 1.f));
+        v.push_back(Mat1f(2, 2, 2.f));
+        to.Append("foo", v);
+        EXPECT_SIZE(1, to.FeatureNames());
+        VecMat1f v2 = to.MostRecent("foo").get<VecMat1f>();
+        EXPECT_SIZE(2, v2);
+        EXPECT_MAT_EQ(Mat1f(2, 1, 1.f), v2[0]);
+        EXPECT_MAT_EQ(Mat1f(2, 2, 2.f), v2[1]);
+    }
+
+    {
+        VecMat1f v;
+        v.push_back(Mat1f(3, 1, 1.f));
+        v.push_back(Mat1f(3, 2, 2.f));
+        v.push_back(Mat1f(3, 3, 3.f));
+        to.Append("bar", v);
+        EXPECT_SIZE(2, to.FeatureNames());
+
+        VecMat1f v2 = to.MostRecent("bar").get<VecMat1f>();
+        EXPECT_SIZE(3, v2);
+        EXPECT_MAT_EQ(Mat1f(3, 1, 1.f), v2[0]);
+        EXPECT_MAT_EQ(Mat1f(3, 2, 2.f), v2[1]);
+        EXPECT_MAT_EQ(Mat1f(3, 3, 3.f), v2[2]);
+
+        v2 = to.MostRecent("foo").get<VecMat1f>();
+        EXPECT_SIZE(2, v2);
+        EXPECT_MAT_EQ(Mat1f(2, 1, 1.f), v2[0]);
+        EXPECT_MAT_EQ(Mat1f(2, 2, 2.f), v2[1]);
+    }
+}
+
+#ifdef __WITH_PCL
+
+TEST_F(SignalTest, AppendCloudXYZ)
+{
+    Signal to; ///< test object
+
+    to.Append("foo", Mat2PointCloud_<pcl::PointXYZ >(Mat1f(1, 3, 1.f)));
+    EXPECT_SIZE(1, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(1, 4, 1.f), to.MostRecentMat1f("foo"));
+
+    to.Append("foo", Mat2PointCloud_<pcl::PointXYZ > (Mat1f(2, 3, 1.f)));
+    EXPECT_SIZE(1, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(2, 4, 1.f), to.MostRecentMat1f("foo"));
+
+    to.Append("bar", Mat2PointCloud_<pcl::PointXYZ > (Mat1f(3, 3, 1.f)));
+    EXPECT_SIZE(2, to.FeatureNames());
+    EXPECT_MAT_EQ(Mat1f(3, 4, 1.f), to.MostRecentMat1f("bar"));
+    EXPECT_MAT_EQ(Mat1f(2, 4, 1.f), to.MostRecentMat1f("foo"));
+}
+
+#endif // __WITH_PCL
