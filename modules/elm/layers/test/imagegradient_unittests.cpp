76f0b2c243eaefcc2fadcb50714f8bcfb2da91e4
Youssef Kashef <youssef.kashef@gmail.com>
2015-04-21 11:09:49 +0200

    test response for image gradient layer

--- modules/elm/layers/test/imagegradient_unittests.cpp
+++ modules/elm/layers/test/imagegradient_unittests.cpp
@@ -17,7 +17,6 @@
 #include "elm/core/signal.h"
 #include "elm/ts/layer_assertions.h"
 #include "elm/ts/mat_assertions.h"
-#include "elm/core/debug_utils.h"
 
 using namespace std;
 using namespace cv;
@@ -119,6 +118,87 @@ TEST_F(ImageGradientTest, Invalid_input)
     }
 }
 
+TEST_F(ImageGradientTest, Gradient)
+{
+    const int ROWS=3;
+    const int COLS=2;
+
+    float data1[ROWS*COLS] = {1.1f,  1.2f,
+                              2.3f,  2.2f,
+                              -3.1f, 0.f,
+                             };
+    Mat1f in = Mat1f(ROWS, COLS, data1).clone();
+
+    Signal sig;
+    sig.Append(NAME_IN, in);
+
+    to_->Activate(sig);
+    to_->Response(sig);
+
+    Mat2f gradient = static_cast<Mat2f>(sig.MostRecentMat1f(NAME_GRAD));
+
+    EXPECT_EQ(ROWS, gradient.rows);
+    EXPECT_EQ(COLS, gradient.cols);
+
+    VecMat1f components;
+    cv::split(gradient, components);
+
+    Mat1f grad_x = components[0];
+    EXPECT_FLOAT_EQ(0.1f, grad_x(0, 0));
+    EXPECT_NEAR(-0.1f, grad_x(1, 0), 1e-5);
+    EXPECT_FLOAT_EQ(-in(2, 0), grad_x(2, 0));
+    EXPECT_MAT_EQ(Mat1f::zeros(ROWS, 1), grad_x.col(COLS-1));
+
+    Mat1f grad_y = components[1];
+    EXPECT_FLOAT_EQ(1.2f, grad_y(0, 0));
+    EXPECT_FLOAT_EQ(1.0f, grad_y(0, 1));
+    EXPECT_FLOAT_EQ(-3.1f-2.3f, grad_y(1, 0));
+    EXPECT_NEAR(-2.2f, grad_y(1, 1), 1e-5);
+    EXPECT_MAT_EQ(Mat1f::zeros(1, COLS), grad_y.row(ROWS-1));
+}
+
+TEST_F(ImageGradientTest, Gradient_nan)
+{
+    const int ROWS=3;
+    const int COLS=2;
+
+    const float NAN_VALUE=std::numeric_limits<float>::quiet_NaN();
+
+    float data1[ROWS*COLS] = {1.1f,  NAN_VALUE,
+                              2.3f,  NAN_VALUE,
+                              -3.1f, 0.f,
+                             };
+    Mat1f in = Mat1f(ROWS, COLS, data1).clone();
+
+    Signal sig;
+    sig.Append(NAME_IN, in);
+
+    to_->Activate(sig);
+    to_->Response(sig);
+
+    Mat2f gradient = static_cast<Mat2f>(sig.MostRecentMat1f(NAME_GRAD));
+
+    EXPECT_EQ(ROWS, gradient.rows);
+    EXPECT_EQ(COLS, gradient.cols);
+
+    VecMat1f components;
+    cv::split(gradient, components);
+
+    Mat1f grad_x = components[0];
+
+    EXPECT_NE(grad_x(0, 0), grad_x(0, 0));
+    EXPECT_NE(grad_x(1, 0), grad_x(1, 0));
+    EXPECT_FLOAT_EQ(-in(2, 0), grad_x(2, 0));
+    EXPECT_MAT_EQ(Mat1f::zeros(ROWS, 1), grad_x.col(COLS-1));
+
+    Mat1f grad_y = components[1];
+
+    EXPECT_FLOAT_EQ(1.2f, grad_y(0, 0));
+    EXPECT_NE(grad_y(0, 1), grad_y(0, 1));
+    EXPECT_FLOAT_EQ(-3.1f-2.3f, grad_y(1, 0));
+    EXPECT_NE(-grad_y(1, 1), grad_y(1, 1));
+    EXPECT_MAT_EQ(Mat1f::zeros(1, COLS), grad_y.row(ROWS-1));
+}
 
 } // annonymous namespace for test cases and fixtures
 
