f09c9baaa7b6e416235043e1fee6bc509f32b1a3
Youssef Kashef <youssef.kashef@gmail.com>
2015-05-04 15:20:53 +0200

    initial addition for base reader layer

---
+++ modules/elm/io/base_reader.cpp
@@ -0,0 +1,79 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#include "elm/io/base_reader.h"
+
+#include <boost/filesystem.hpp>
+
+#include "elm/core/exception.h"
+#include "elm/core/layerconfig.h"
+
+using std::string;
+namespace bfs=boost::filesystem;
+using namespace elm;
+
+const string base_Reader::PARAM_PATH = detail::BASE_READER_PARAM_PATH;
+
+base_Reader::~base_Reader()
+{
+}
+
+base_Reader::base_Reader()
+    : base_Layer()
+{
+}
+
+base_Reader::base_Reader(const LayerConfig &cfg)
+    : base_Layer(cfg)
+{
+}
+
+void base_Reader::Clear() {
+
+    // do nothing
+}
+
+void base_Reader::Reconfigure(const LayerConfig& config) {
+
+    PTree params = config.Params();
+
+    bfs::path _path = params.get<bfs::path>(PARAM_PATH);
+
+    if(bfs::exists(_path)) {
+
+        std::stringstream s;
+        s << "Path does not exist (" << _path << ")";
+        ELM_THROW_FILEIO_ERROR(s.str());
+    }
+
+    int nb_items = ReadHeader(_path.string());
+
+    if(nb_items < 1) {
+
+        std::stringstream s;
+        s << "No items found under (" << _path << ")";
+        ELM_THROW_FILEIO_ERROR(s.str());
+    }
+}
+
+void base_Reader::InputNames(const LayerInputNames& io) {
+
+    // do nothing
+}
+
+void base_Reader::Activate(const Signal &signal) {
+
+    // do nothing
+}
+
+void base_Reader::Response(Signal &signal) {
+
+    if(!IS_EOF()) {
+
+        Next();
+    }
+}
---
+++ modules/elm/io/base_reader.h
@@ -0,0 +1,69 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#ifndef _ELM_IO_BASE_READER_H_
+#define _ELM_IO_BASE_READER_H_
+
+#include <string>
+
+#include "elm/core/base_Layer.h"
+
+namespace elm {
+
+
+namespace detail {
+
+const std::string BASE_READER_PARAM_PATH = "path";
+
+}
+
+class base_Reader : public base_Layer
+{
+public:
+    static const std::string PARAM_PATH;    ///< path parameter
+
+    /**
+     * @brief read file header
+     * @param path
+     * @return no. of items
+     */
+    virtual int ReadHeader(const std::string &path) = 0;
+
+    /**
+     * @brief advance to next item
+     */
+    virtual void Next() = 0;
+
+    /**
+     * @brief Check if we've reached the end of the file
+     * @return true on end of file reached
+     */
+    virtual bool IS_EOF() const = 0;
+
+    virtual void Clear();
+
+    virtual void Reconfigure(const LayerConfig& config);
+
+    virtual void InputNames(const LayerInputNames& io);
+
+    virtual void OutputNames(const LayerOutputNames& io) = 0;
+
+    virtual void Activate(const Signal &signal);
+
+    virtual void Response(Signal &signal);
+
+protected:
+    virtual ~base_Reader();
+
+    base_Reader();
+
+    base_Reader(const LayerConfig &cfg);
+};
+
+} // namespace elm
+
+#endif // _ELM_IO_BASE_READER_H_
