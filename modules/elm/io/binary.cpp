bd10ddd627998db0a8cac820d232d181a2ed8306
Youssef Kashef <youssef.kashef@gmail.com>
2015-05-06 10:38:58 +0200

    remove duplicate implementation

--- modules/elm/io/binary.cpp
+++ modules/elm/io/binary.cpp
@@ -7,11 +7,3 @@
 //M*/
 #include "elm/io/binary.h"
 
-#include <algorithm>
-
-template <class T>
-void SwapEndian(T *p)
-{
-    unsigned char *mem = reinterpret_cast<unsigned char*>(p);
-    std::reverse(mem, mem + sizeof(T));
-}
