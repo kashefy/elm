576014b44d71b7e624456148eb99d12d5bc4d07f
Youssef Kashef <youssef.kashef@gmail.com>
2015-05-07 15:30:24 +0200

    configure git user for travis, generate doxygen docs, push to gh-pages

--- ci/travis/docs_to_gh-pages.sh
+++ ci/travis/docs_to_gh-pages.sh
@@ -15,3 +15,19 @@ skip "Skipping docs generation builds triggered by pull-requests."
 # limit to specific branches
 #[ "${TRAVIS_BRANCH}" = "master" ] || \
 #skip "Limit docs generation to pushes to 'master' branch (current branch is: ${TRAVIS_BRANCH})."
+
+cd $TRAVIS_BUILD_DIR
+doxygen ./docs/doxygenConf.txt
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/kashefy/elm gh-pages > /dev/null
+
+cd gh-pages
+git rm -rf docs
+mkdir -p docs/
+cp -Rf $TRAVIS_BUILD_DIR/docs/html ./docs/html
+git add -f .
+git commit -m "[gh-pages] [docs] auto-push updated doxygen docs on successful travis build $TRAVIS_BUILD_NUMBER to gh-pages"
+git push -fq origin gh-pages
+echo -e "Published doxygen docs to gh-pages."
