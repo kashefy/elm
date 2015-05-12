bb1b853e712f6e1e1003d3f88abeb8d158a45985
Youssef Kashef <youssef.kashef@gmail.com>
2015-05-12 13:52:29 +0200

    move stl template instantiation to its own source file

--- modules/elm/core/stl/extern.cpp
+++ modules/elm/core/stl/extern.cpp
@@ -5,9 +5,9 @@
 // 3-clause BSD License
 //
 //M*/
-#include "elm/core/stl/stl_inl.h"
+#include "elm/core/stl/typedefs.h"
 
-using namespace elm;
-
-
-
+template class std::vector<std::string>;
+template class std::vector<float>;
+template class std::vector<int>;
+template class std::map<std::string, std::string>;
