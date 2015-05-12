147ca299dfd0e36ae041cfcbb56d34c0cabd7ef9
Youssef Kashef <youssef.kashef@gmail.com>
2015-05-12 13:56:18 +0200

    move cv template instantiations to its own source file

---
+++ modules/elm/core/cv/extern.cpp
@@ -0,0 +1,21 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#include "elm/core/cv/typedefs_fwd.h"
+
+#include <opencv2/core/core.hpp>
+
+template class cv::Mat_<float>;
+template class cv::Mat_<int>;
+template class cv::Mat_<uchar>;
+
+template class cv::SparseMat_<float>;
+
+template class cv::Rect_<int>;
+template class cv::Size_<int>;
+template class cv::Point_<int>;
+template class cv::Point3_<int>;
