7e242f3607893925016905d1a372e4d21f434e8c
Youssef Kashef <youssef.kashef@gmail.com>
2015-05-12 13:50:09 +0200

    move pcl template instantiation to its own source file

---
+++ modules/elm/core/pcl/extern.cpp
@@ -0,0 +1,12 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#include "elm/core/pcl/cloud_.h"
+
+template class pcl::PointCloud<pcl::PointXYZ >;
+template class pcl::PointCloud<pcl::Normal >;
+template class pcl::PointCloud<pcl::PointNormal >;
