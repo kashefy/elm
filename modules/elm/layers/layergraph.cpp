b17c9846b14d46509693a4cdb5105b3be977c864
Youssef Kashef <youssef.kashef@gmail.com>
2015-05-26 16:40:46 +0200

    implement external interface of LayerGraph Pimpl idiom

--- modules/elm/layers/layergraph.cpp
+++ modules/elm/layers/layergraph.cpp
@@ -24,3 +24,41 @@ LayerGraph::LayerGraph()
     : impl_(new LayerGraph_Impl)
 {
 }
+
+void LayerGraph::Add(const std::string &name,
+                     LayerShared &layer,
+                     const LayerConfig &cfg,
+                     const LayerIONames &io) {
+
+    impl_->Add(name, layer, cfg, io);
+}
+
+void LayerGraph::AddOutput(const std::string &output_name) {
+
+    impl_->AddOutput(output_name);
+}
+
+SetS LayerGraph::Outputs() const {
+
+    return impl_->Outputs();
+}
+
+SetS LayerGraph::Inputs() const {
+
+    return impl_->Inputs();
+}
+
+void LayerGraph::ClearActive() {
+
+    impl_->ClearActive();
+}
+
+void LayerGraph::Configure() {
+
+    impl_->Configure();
+}
+
+void LayerGraph::Sequence(std::vector<LayerShared> &layer_seq) {
+
+    impl_->Sequence(layer_seq);
+}
