76ff496e880571fa211b26794c7fd3a01ab14d78
Youssef Kashef <youssef.kashef@gmail.com>
2015-05-31 15:25:57 +0200

    add source file to instantiate template objects

---
+++ modules/elm/layers/extern.cpp
@@ -0,0 +1,13 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#include "elm/core/stl/typedefs.h"
+
+template class std::vector<std::string>;
+template class std::vector<float>;
+template class std::vector<int>;
+template class std::map<std::string, std::string>;
