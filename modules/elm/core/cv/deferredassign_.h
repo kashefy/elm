67e1f594ca229f1c33a315ea89699022ab1827d3
Youssef Kashef <youssef.kashef@gmail.com>
2015-06-02 17:54:35 +0200

    add missing license header

--- modules/elm/core/cv/deferredassign_.h
+++ modules/elm/core/cv/deferredassign_.h
@@ -1,3 +1,10 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
 #ifndef _ELM_CORE_CV_DEFERREDASSIGN__H_
 #define _ELM_CORE_CV_DEFERREDASSIGN__H_
 
