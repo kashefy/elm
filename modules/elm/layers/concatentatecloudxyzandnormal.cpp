2428143eeb3183df2be55cf2061ec60596749528
Youssef Kashef <youssef.kashef@gmail.com>
2015-06-03 12:41:58 +0200

    layer for concatenating xyz and normal point clouds

---
+++ modules/elm/layers/concatentatecloudxyzandnormal.cpp
@@ -0,0 +1,85 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#include "elm/layers/concatentatecloudxyzandnormal.h"
+
+#ifdef __WITH_PCL   // the layer is otherwise implemented as unsupported
+
+#include <pcl/point_types.h>
+
+#include "elm/core/featuredata.h"
+#include "elm/core/inputname.h"
+#include "elm/core/layerinputnames.h"
+#include "elm/core/layeroutputnames.h"
+#include "elm/core/signal.h"
+#include "elm/ts/layerattr_.h"
+
+using namespace std;
+using namespace pcl;
+using namespace elm;
+
+extern template class pcl::PointCloud<pcl::PointXYZ >;
+extern template class pcl::PointCloud<pcl::Normal >;
+extern template class pcl::PointCloud<pcl::PointNormal >;
+
+extern template class boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ > >;
+extern template class boost::shared_ptr<pcl::PointCloud<pcl::Normal > >;
+extern template class boost::shared_ptr<pcl::PointCloud<pcl::PointNormal > >;
+
+const string ConcatentateCloudXYZAndNormal::KEY_INPUT_XYZ       = "xyz";
+const string ConcatentateCloudXYZAndNormal::KEY_INPUT_NORMAL    = "normal";
+const string ConcatentateCloudXYZAndNormal::KEY_OUTPUT          = "output";
+
+#include <boost/assign/list_of.hpp>
+template <>
+elm::MapIONames LayerAttr_<ConcatentateCloudXYZAndNormal>::io_pairs = boost::assign::map_list_of
+        ELM_ADD_INPUT_PAIR(ConcatentateCloudXYZAndNormal::KEY_INPUT_XYZ)
+        ELM_ADD_INPUT_PAIR(ConcatentateCloudXYZAndNormal::KEY_INPUT_NORMAL)
+        ELM_ADD_OUTPUT_PAIR(ConcatentateCloudXYZAndNormal::KEY_OUTPUT)
+        ;
+
+ConcatentateCloudXYZAndNormal::ConcatentateCloudXYZAndNormal()
+    : base_Layer()
+{
+}
+
+void ConcatentateCloudXYZAndNormal::Clear()
+{
+    dst_cloud_.reset();
+}
+
+void ConcatentateCloudXYZAndNormal::Reconfigure(const LayerConfig &cfg)
+{
+}
+
+void ConcatentateCloudXYZAndNormal::InputNames(const LayerInputNames &in_names) {
+
+    name_xyz_ = in_names.Input(KEY_INPUT_XYZ);
+    name_norml_ = in_names.Input(KEY_INPUT_NORMAL);
+}
+
+void ConcatentateCloudXYZAndNormal::OutputNames(const LayerOutputNames &out_names) {
+
+    name_out_  = out_names.Output(KEY_OUTPUT);
+}
+
+void ConcatentateCloudXYZAndNormal::Activate(const Signal &signal)
+{
+    CloudXYZPtr xyz = signal.MostRecent(name_xyz_).get<CloudXYZPtr>();
+    CloudNrmlPtr nrml = signal.MostRecent(name_norml_).get<CloudNrmlPtr>();
+
+    // Concatenate the XYZ and normal fields
+    dst_cloud_.reset(new PointCloud<PointNormal>);
+    concatenateFields(*xyz, *nrml, *dst_cloud_);
+}
+
+void ConcatentateCloudXYZAndNormal::Response(Signal &signal)
+{
+    signal.Append(name_out_, dst_cloud_);
+}
+
+#endif // __WITH_PCL
---
+++ modules/elm/layers/concatentatecloudxyzandnormal.h
@@ -0,0 +1,61 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, Youssef Kashef
+// Copyright (c) 2015, ELM Library Project
+// 3-clause BSD License
+//
+//M*/
+#ifndef _ELM_LAYERS_CONCATENTATECLOUDXYZANDNORMAL_H_
+#define _ELM_LAYERS_CONCATENTATECLOUDXYZANDNORMAL_H_
+
+#ifndef __WITH_PCL
+    // Disabling ConcatentateCloudXYZAndNormal layer due to no PCL support and defining it as a non-supported
+    #include "elm/layers/layernotsupported.h"
+namespace elm {
+    class ConcatentateCloudXYZAndNormal : public ELM_LAYER_NOT_SUPPORTED(ConcatentateCloudXYZAndNormal, "Building with PCL is required for supporting ConcatentateCloudXYZAndNormal layer.");
+} // namespace elm
+#else   // __WITH_PCL
+
+#include <string>
+
+#include <boost/shared_ptr.hpp>
+
+#include "elm/core/pcl/typedefs_fwd.h"
+#include "elm/core/base_Layer.h"
+
+namespace elm {
+
+class ConcatentateCloudXYZAndNormal : base_Layer
+{
+public:
+    static const std::string KEY_INPUT_XYZ;     ///< key to xyz cloud
+    static const std::string KEY_INPUT_NORMAL;  ///< key to normal cloud
+    static const std::string KEY_OUTPUT;        ///< key output
+
+    ConcatentateCloudXYZAndNormal();
+
+    void Clear();
+
+    void Reconfigure(const LayerConfig &cfg);
+
+    void InputNames(const LayerInputNames &in_names);
+
+    void OutputNames(const LayerOutputNames &out_names);
+
+    void Activate(const Signal &signal);
+
+    void Response(Signal &signal);
+
+protected:
+    std::string name_xyz_;      ///< name of xyz input in signal object
+    std::string name_norml_;    ///< name of normal input in signal object
+    std::string name_out_;      ///< desitnation name of output cloud
+
+    CloudPtNrmlPtr dst_cloud_;
+};
+
+} // namespace elm
+
+#endif   // __WITH_PCL
+
+#endif // _ELM_LAYERS_CONCATENTATECLOUDXYZANDNORMAL_H_
