303ceacc43eec828227e7fa24b5552e6d10faf8c
travis-ci <travis@travis-ci.org>
2015-06-05 13:40:22 +0000

    [gh-pages] [docs] auto-push updated doxygen docs on successful travis build 538 to gh-pages

--- docs/html/classelm_1_1Exception.js
+++ docs/html/classelm_1_1Exception.js
@@ -1,4 +1,14 @@
 var classelm_1_1Exception =
 [
-    [ "Exception", "classelm_1_1Exception.html#a30a647b10a452420b3663390f9a97c72", null ]
+    [ "Exception", "classelm_1_1Exception.html#a25442546d4f70c3b622c1edea0efe4ed", null ],
+    [ "Exception", "classelm_1_1Exception.html#adfa309dfcc7a079a089615e6bda38a11", null ],
+    [ "~Exception", "classelm_1_1Exception.html#a3b16846394bdf90226b416c5ffeeab1b", null ],
+    [ "formatMessage", "classelm_1_1Exception.html#a29d3f117d84767a0273fc810b790e089", null ],
+    [ "what", "classelm_1_1Exception.html#aaa6a5a82770b71154043e110481c7a02", null ],
+    [ "code", "classelm_1_1Exception.html#aae8c6a244178bbf87dcd506f6b8487eb", null ],
+    [ "err", "classelm_1_1Exception.html#a4489381518faded9cbc41dc776e0a607", null ],
+    [ "file", "classelm_1_1Exception.html#adf30944a44d0259ce85199b0acdb2157", null ],
+    [ "func", "classelm_1_1Exception.html#acc36b634d3b165146eedd67b11130d94", null ],
+    [ "line", "classelm_1_1Exception.html#a25f9813765f4ee65d794bb80ca1e7bc4", null ],
+    [ "msg", "classelm_1_1Exception.html#ab31b031fb87f8ae87209dd84b5a1896c", null ]
 ];
