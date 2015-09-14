e6a9137dd98f383bbf57850d08d1e42e19927613
Youssef Kashef <youssef.kashef@gmail.com>
2015-09-14 19:13:08 +0200

    more debug messages

--- cmake/DetectOpenCV.cmake
+++ cmake/DetectOpenCV.cmake
@@ -6,7 +6,7 @@
 # ----------------------------------------------------------------------------
 if(DEFINED OpenCV_DIR)
     get_filename_component(OpenCV_DIR_TYPE ${OpenCV_DIR} EXT)
-
+    message(STATUS "OpenCV_DIR_EXT${OpenCV_DIR_EXT}")
     if(OpenCV_DIR_EXT STREQUAL ".pc")
 
         message(STATUS "PC file ${OpenCV_DIR}")
@@ -18,10 +18,14 @@ if(DEFINED OpenCV_DIR)
         pkg_check_modules(OpenCV opencv)
 
     else(OpenCV_DIR_EXT STREQUAL ".pc")
+
+    message(STATUS "OpenCV_DIR not .pc")
         find_package(OpenCV REQUIRED core highgui imgproc ml PATHS ${OpenCV_DIR})
     endif(OpenCV_DIR_EXT STREQUAL ".pc")
 
 else(DEFINED OpenCV_DIR)
+
+    message(STATUS "OpenCV_DIR not defined")
     find_package(OpenCV REQUIRED core highgui imgproc ml)
 endif(DEFINED OpenCV_DIR)
 
